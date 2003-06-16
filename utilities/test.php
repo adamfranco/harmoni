1f2fb681f25dbfa79b6830b1acd1b8d62166ce51
adamfranco <adamfranco>
2003-06-16 20:35:51 +0000

    adding a directory for common utility classes, such as Queue

---
+++ utilities/test.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * A group test template using the SimpleTest unit testing package.
+ * Just add the UnitTestCase files below using addTestFile().
+ *
+ * @author Dobo Radichkov
+ * @version $Id: test.php,v 1.1 2003/06/16 20:35:51 adamfranco Exp $
+ * @copyright 2003 
+ **/
+
+    if (!defined('SIMPLE_TEST')) {
+        define('SIMPLE_TEST', '../../simple_test/');
+    }
+    require_once(SIMPLE_TEST . 'simple_unit.php');
+    require_once(SIMPLE_TEST . 'dobo_simple_html_test.php');
+
+    $test =& new GroupTest('All tests');
+    $test->addTestFile('../tests/TypedUserTestCase.class.php');
+    $test->addTestFile('../tests/GroupTestCase.class.php');
+    $test->attachObserver(new DoboTestHtmlDisplay());
+    $test->run();
+
+?>
