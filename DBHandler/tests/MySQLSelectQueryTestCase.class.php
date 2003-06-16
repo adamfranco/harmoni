1c9c71432f2b9fc07210642d1fe89ebe981666dc
dobomode <dobomode>
2003-06-16 22:10:32 +0000

    Work started on DBHandler.

---
+++ DBHandler/tests/MySQLSelectQueryTestCase.class.php
@@ -0,0 +1,48 @@
+<?php
+
+    require_once('MySQLSelectQuery.class.php');
+
+/**
+ * A single unit test case. This class is intended to test one particular
+ * class. Replace 'testedclass.php' below with the class you would like to
+ * test.
+ *
+ * @author Dobo Radichkov
+ * @version $Id: MySQLSelectQueryTestCase.class.php,v 1.1 2003/06/16 22:10:33 dobomode Exp $
+ * @copyright 2003 
+ **/
+
+    class MySQLSelectQueryTestCase extends UnitTestCase {
+	
+
+		function MySQLSelectQueryTestCase() {
+			$this->UnitTestCase();
+		}
+
+        /**
+         *    Sets up unit test wide variables at the start
+         *    of each test method.
+         *    @public
+         */
+        function setUp() {
+			// perhaps, initialize $obj here
+        }
+		
+        /**
+         *    Clears the data set in the setUp() method call.
+         *    @public
+         */
+        function tearDown() {
+			// perhaps, unset $obj here
+        }
+
+		/**
+		 *    
+		 */ 
+        function test() {
+			
+		}
+		
+    }
+
+?>
