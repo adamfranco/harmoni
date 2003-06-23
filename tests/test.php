53a62843dd10e4eca1013aeacbdcf70bed05051c
adamfranco <adamfranco>
2003-06-23 21:20:54 +0000

    creating a global test script

---
+++ tests/test.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * A group test template using the SimpleTest unit testing package.
+ * Just add the UnitTestCase files below using addTestFile().
+ *
+ * @version $Id: test.php,v 1.1 2003/06/23 21:20:54 adamfranco Exp $
+ * @copyright 2003 
+ **/
+
+require_once("../harmoni.inc.php");
+require_once(HARMONI."DBHandler/classes/DBHandler.class.php");
+
+    if (!defined('SIMPLE_TEST')) {
+        define('SIMPLE_TEST', HARMONI.'simple_test/');
+    }
+    require_once(SIMPLE_TEST . 'simple_unit.php');
+    require_once(SIMPLE_TEST . 'dobo_simple_html_test.php');
+
+    $test =& new GroupTest('All tests');
+    $test->addTestFile(HARMONI.'Services/tests/test.php');
+    $test->addTestFile(HARMONI.'DBHandler/tests/test.php');
+    $test->addTestFile(HARMONI.'errorHandler/tests/test.php');
+    $test->addTestFile(HARMONI.'utilities/tests/test.php');
+    $test->attachObserver(new DoboTestHtmlDisplay());
+    $test->run();
+
+?>
