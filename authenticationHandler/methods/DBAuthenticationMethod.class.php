6d5494a98b95723d3a99ec1ae3e18a58fd28a10b
gabeschine <gabeschine>
2003-06-25 14:05:17 +0000

    added DBAuthenticationMethod

--- authenticationHandler/methods/DBAuthenticationMethod.class.php
+++ authenticationHandler/methods/DBAuthenticationMethod.class.php
@@ -1,10 +1,13 @@
 <?php
 
+require_once(HARMONI."authenticationHandler/AuthenticationMethod.abstract.php");
+require_once(HARMONI."utilities/DataContainer.abstract.php");
+
 /**
  * the DB Authentication Method will contact an SQL database and check a username/password pair
  * against fields in a specified table.
  *
- * @version $Id: DBAuthenticationMethod.class.php,v 1.1 2003/06/23 13:22:53 gabeschine Exp $
+ * @version $Id: DBAuthenticationMethod.class.php,v 1.2 2003/06/25 14:07:50 gabeschine Exp $
  * @copyright 2003 
  * @access public
  * @package harmoni.authenticationHandler
@@ -13,20 +16,44 @@
 class DBAuthenticationMethod
 	extends AuthenticationMethod 
 {
+	/**
+	 * The options passed to us by the user.
+	 * @access private
+	 * @var object DBMethodOptions $_opt
+	 **/
+	var $_opt;
+	
+	/**
+	 * The DBHandler we will use for DB connectivity.
+	 * @access private
+	 * @var object DBHandler $_DBHandler
+	 **/
+	var $_DBHandler;
+	
+	/**
+	 * The connection ID for our DB connection.
+	 * @access private
+	 * @var int $_id
+	 **/
+	var $_id;
+	
 	
 	/**
 	 * the constructor
 	 *
 	 * @param string $name the unique name for this module
-	 * @param object FieldSet $options a FieldSet object with all the necessary options for this module
+	 * @param object DBMethodOptions $options a FieldSet object with all the necessary options for this module
 	 * @access public
 	 * @return void
 	 **/
 	function DBAuthenticationMethod ( $name, & $options ) {
 		$this->_name = $name;
 		
-		// parse the options
-		// @todo -cDBAuthenticationMethod constructor - parse the options
+		// store the options
+		$this->_opt = & $options;
+		
+		// get the DBHandler
+		$this->_DBHandler = & Services::getService("DBHandler");
 	}
 
 	/**
@@ -38,9 +65,221 @@ class DBAuthenticationMethod
 	 * @return boolean true if authentication succeeded with the method, false if not 
 	 **/
 	function authenticate( $systemName, $password ) {
-		// @todo -cDBAuthenticationMethod implement authenticate to contact the DB, etc
+		if (!$id = $this->_connect()) return false;
+		if ($this->agentExists($systemName)) {
+			// they exist, now check their password
+			
+			// get the encrypted version of the pwd we got passed
+			$ourPass = $this->_getEncryptedPassword($systemName,$password);
+			
+			// now get the one from the DB
+			$a = $this->_fetchFieldFromDB( $systemName, $o->get("passwordField"));
+			$dbPass = $a[$o->get("passwordField")];
+			
+			// disconnect
+			$this->_disconnect();
+			
+			// now compare the two
+			if ($ourPass == $dbPass) // they're good!
+				return true;
+			else					// or not...
+				return false; // denied.
+		}
 	}
 	
+	/**
+	 * Encrypts $password using the method specified by the user.
+	 * @param string $systemName The users's system name.
+	 * @param string $password The password to encrypt.
+	 * @access public
+	 * @return string The encrypted version of $password.
+	 **/
+	function _getEncryptedPassword( $systemName, $password ) {
+		$o = & $this->_opt;
+		// check if the password in the DB is encrypted
+		if ($o->get("passwordFieldEncrypted")) {
+			switch($o->get("passwordFieldEncryptionType")){
+				case "database":
+					$passwordQuery = & new SelectQuery;
+					$passwordQuery->addColumn("PASSWORD('".$password."')","encryptedPassword");
+					$passwordResult = & $this->_DBHandler->query($passwordQuery,$this->_id);
+					return $passwordResult->field("encryptedPassword");
+					break;
+				case "crypt": 
+					$r = $this->_getFieldsFromDB($systemName,$o->get("passwordField"));
+					$dbPassword = $r[$o->get("passwordField")];
+					return crypt($password,substr($dbPassword, 0, CRYPT_SALT_LENGTH));
+					break;
+				default:
+					return $password;
+			} // switch
+		}
+		return $password;
+	}
+	
+	/**
+	 * Fetches the specified fields from the DB form $systemName.
+	 * @param string $systemName The system name to fetch info for.
+	 * @param string $field1,... List of fields to fetch.
+	 * @access public
+	 * @return array An associative array of fields: [field1]=>data1,... 
+	 **/
+	function _getFieldsFromDB( $systemName ) {
+		if (func_num_args() < 2) return array();
+		
+		$toFetch = array();
+		for ($i = 1; $i < func_num_args(); $i++)
+			$toFetch[] = func_get_arg($i);
+		
+		// get the options
+		$o = & $this->_opt;
+		
+		// get the DBHandler
+		$DBHandler = & $this->_DBHandler;
+		
+		$query = & new SelectQuery;
+		$query->addTable($o->get("tableName"));
+		
+		foreach ($toFetch as $field) {
+			if (is_array($field))
+				foreach ($field as $f) $query->addColumn($f);
+			else
+				$query->addColumn($field);
+		}
+		
+		$where = $o->get("userNameField")."='" . $systemName ."'";
+		$query->setWhere($where);
+		
+		$result = & $DBHandler->query($query,$this->_id);
+		if ($result->getNumberOfRows()) {
+			return $result->getCurrentRow();
+		}
+		return array();
+	}
+	
+	
+	/**
+	 * connects to the database
+	 * @access private
+	 * @return int The connection ID. 
+	 **/
+	function _connect( ) {
+		
+		// if we already connected this session, just return
+		if ($this->_id) return $this->_id;
+		
+		$o = &$this->_opt;
+		$DBHandler = & $this->_DBHandler;
+		$id = $DBHandler->createDatabase(
+				$o->get("databaseType"),
+				$o->get("databaseHost"),
+				$o->get("databaseName"),
+				$o->get("databaseUsername"),
+				$o->get("databasePassword"));
+		// persistent connect
+		$DBHandler->pConnect($id);
+		
+		// check if we're connected
+		if (!$DBHandler->isConnected($id)) {
+			// @todo -cDBAuthenticationMethod throw an error!
+			return false;
+		}
+		$this->_id = $id;
+		return $id;
+	}
+	
+	/**
+	 * disconnects from the DB
+	 * @param object DBHandler $DBHandler The DBHandler to disconnect with.
+	 * @param int $id The ID of the connection.
+	 * @access private
+	 * @return void 
+	 **/
+	function _disconnect( ) {
+		if ($this->_id)$this->_DBHandler->disconnect($this->_id);
+	}
+	
+	/**
+	 * @access public
+	 * @return string returns the user-defined name/ID of the module
+	 **/
+	function getName () {
+		return $this->_name;
+	}
+	
+	/**
+	 * Get's information for $systemName (could be, for example, full name, email, etc)
+	 * 
+	 * @param string $systemName The system name to get info for.
+	 * @access public
+	 * @return array An associative array of [key]=>value pairs. 
+	 **/
+	function getAgentInformation( $systemName ) {
+		if (!$id = $this->_connect()) return false;
+		
+		// what fields do we need to get?
+		$o = & $this->_opt;
+		$fields = $o->get("agentInformationFields");
+		if (!is_array($fields) || !count($fields)) return array();
+		
+		$results = $this->_fetchFieldsFromDB($systemName,$fields);
+		
+		// make a [key]=>value array for the results
+		$info = array();
+		foreach ($fields as $key=>$val) {
+			$info[$key] = $results[$val];
+		}
+		
+		$this->_disconnect();
+		return $info;
+	}
+	
+	/**
+	 * Checks to see if $systemName exists in this method.
+	 * 
+	 * @param string $systemName The system name to check.
+	 * @access public
+	 * @return boolean If the agent exists or not. 
+	 **/
+	function agentExists( $systemName ) {
+		if (!$id = $this->_connect()) return false;
+
+		// get the options
+		$o = & $this->_opt;
+		
+		// get the DBHandler
+		$DBHandler = & $this->_DBHandler;
+		
+		$query = & new SelectQuery;
+		$query->addTable($o->get("tableName"));
+		
+		$query->addColumn($o->get("userNameField"));
+		
+		$where = $o->get("userNameField")."='" . $systemName ."'";
+		$query->setWhere($where);
+		
+		$result = & $DBHandler->query($query,$this->_id);
+		$this->_disconnect();
+		if ($result->getNumberOfRows()) // yep
+			return true;
+		return false;
+	}
+}
+
+/** 
+ * 
+ * @package harmoni.authenticationHandler
+ *
+ **/
+class DBMethodOptions extends DataContainer {
+	/**
+     * Constructor
+     * @access protected
+	 * @return void
+     */
+	function DBMethodOptions(){
+		
+	}
 }
 
 ?>
--- configHandler/inc.php
+++
@@ -1,8 +0,0 @@
-<?
-
-// include all the necessary files
-
-require_once(dirname(__FILE__)."/core/configHandler.class.php");
-
-// requires the errorHandler
-//require_once(dirname(__FILE__)."/../errorHandler/inc.php");
