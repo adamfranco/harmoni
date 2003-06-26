32a4fa3fca30c9f930b7f75842a96a37450c9746
gabeschine <gabeschine>
2003-06-26 03:58:09 +0000

    changed EmailValidatorRule to only accept emails like: gabe@server.bla.com and not gabe@server

--- utilities/FieldSetValidator/rules/EmailValidatorRule.class.php
+++ utilities/FieldSetValidator/rules/EmailValidatorRule.class.php
@@ -5,7 +5,7 @@ require_once(HARMONI."utilities/FieldSetValidator/rules/RegexValidatorRule.class
 /**
  * a EmailValidatorRule checks a given value to make sure it is an email address
  *
- * @version $Id: EmailValidatorRule.class.php,v 1.2 2003/06/23 20:59:14 gabeschine Exp $
+ * @version $Id: EmailValidatorRule.class.php,v 1.3 2003/06/26 03:58:09 gabeschine Exp $
  * @copyright 2003 
  * @package harmoni.utilities.FieldSetValidator
  **/
@@ -20,7 +20,7 @@ class EmailValidatorRule
 	 * @return void 
 	 **/
 	function EmailValidatorRule( ) {
-		$this->_regex = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*$"; // matches any string that is an email address
+		$this->_regex = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)+$"; // matches any string that is an email address
 	}
 }
 
