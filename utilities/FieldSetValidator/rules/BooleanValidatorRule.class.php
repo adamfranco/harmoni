c3a690d7d7246d60ca4da35b8e533310cc86f686
gabeschine <gabeschine>
2003-06-26 03:55:47 +0000

    trying to fix bug in BooleanValidatorRule

--- utilities/FieldSetValidator/rules/BooleanValidatorRule.class.php
+++ utilities/FieldSetValidator/rules/BooleanValidatorRule.class.php
@@ -5,7 +5,7 @@ require_once(HARMONI."utilities/FieldSetValidator/rules/ValidatorRule.interface.
 /**
  * the BooleanValidatorRule checks a given value to make sure it's a boolean value
  *
- * @version $Id: BooleanValidatorRule.class.php,v 1.2 2003/06/23 20:59:14 gabeschine Exp $
+ * @version $Id: BooleanValidatorRule.class.php,v 1.3 2003/06/26 03:55:47 gabeschine Exp $
  * @copyright 2003 
  * @package harmoni.utilities.FieldSetValidator
  **/
@@ -20,8 +20,8 @@ class BooleanValidatorRule
 	 * @return boolean true if the value is boolean, false if it is not
 	 **/
 	function check( & $val ) {
-		if (!($val === true || $val === false || $val == 1 || $val == 0)) return false;
-		return true;
+		if ($val === true || $val === false || $val === 1 || $val === 0) return true;
+		return false;
 	}
 }
 
