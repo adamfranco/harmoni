e743a4bdeca012a9f34d234c41ea428fdd7d693f
movsjani <movsjani>
2003-07-10 23:04:47 +0000

    debugged Statistics Handler

--- utilities/StatisticsHandler.class.php
+++ utilities/StatisticsHandler.class.php
@@ -1,14 +1,15 @@
 <?php
 
+require_once(HARMONI.'utilities/StatisticsHandler.interface.php');
 /**
  * An interface to calculate various statistical information.
  *
- * @version $Id: StatisticsHandler.class.php,v 1.1 2003/07/10 22:12:08 movsjani Exp $
+ * @version $Id: StatisticsHandler.class.php,v 1.2 2003/07/10 23:04:47 movsjani Exp $
  * @package harmoni.utilities
  * @copyright 2003 
  */
 
-class StatisticsHandlerInterface {
+class StatisticsHandler extends StatisticsHandlerInterface{
 
 	var $_data;
 	var $_sortedData;
@@ -16,12 +17,12 @@ class StatisticsHandlerInterface {
 	var $_mean, $_median, $_discrimination, $_standardDeviation;
 
  	/**
-	 * Create a Add a handler with given information.
+	 * Create a handler with given data.
 	 * @param object $data The object, which contains all the data. It can either be an array or should be provided with methods: next() and getNext().
 	 * @access public
 	 */	
-	function statisticsHandler(& $data) { 
-		if(isarray($data))
+	function StatisticsHandler(& $data) { 
+		if(is_array($data))
 			$this->_data = $data;
 		
 		else{
@@ -34,7 +35,7 @@ class StatisticsHandlerInterface {
 
 	/**
      * Return the mean value of the data.
-	 * @return integer The mean value of the data.
+	 * @return float The mean value of the data.
 	 * @access public
 	 */
 	function getMean() { 
@@ -43,7 +44,7 @@ class StatisticsHandlerInterface {
 			foreach($this->_data as $value)
 				$sum += $value;
 
-			$sum = round($sum*1.0/$this->_data->getSize(),3);
+			$sum = round($sum/count($this->_data),3);
 			
 			$this->_mean = $sum;
 		}
@@ -53,15 +54,17 @@ class StatisticsHandlerInterface {
 
 	/**
      * Return the median value of the data.
-	 * @return integer The median value of the data.
+	 * @return float The median value of the data.
 	 * @access public
 	 */
 	function getMedian() { 
 		if(!isset($this->_median)){
-			if(!isset($this->_sortedData))
-				$this->_sortedData = sort($this->_data);
+			if(!isset($this->_sortedData)){
+				$this->_sortedData = $this->_data;
+				sort($this->_sortedData);
+			}
 			
-			$this->_median = $this->_sortedData[count->($this->_sortedData)/2];
+			$this->_median = $this->_sortedData[count($this->_sortedData)/2];
 		}
 
 		return $this->_median;
@@ -71,7 +74,7 @@ class StatisticsHandlerInterface {
 
 	/**
      * Return the standard deviation of the data.
-	 * @return integer The standard deviation of  the data.
+	 * @return float The standard deviation of  the data.
 	 * @access public
 	 */
 	function getStandardDeviation() { 
@@ -83,7 +86,7 @@ class StatisticsHandlerInterface {
 			foreach($this->_data as $value)
 				$variance += ($mean-$value)*($mean-$value);
 
-			$this->_standardDeviation = round(sqrt($variance),3);
+			$this->_standardDeviation = round(sqrt($variance/count($this->_data)),3);
 		}
 
 		return $this->_standardDeviation;
@@ -91,11 +94,12 @@ class StatisticsHandlerInterface {
 
 	/**
      * Return the modal (most frequent value) of the data.
-	 * @return integer The modal (most frequent value) of the data.
+	 * @return float The modal (most frequent value) of the data.
 	 * @access public
 	 */
 	function getModal() { 
 		if (!isset($this->_modal)) {
+			$cf=0; $mf=0; $current = '';
 			foreach ($this->_data as $value) {
 				if ($value == $current)
 					$cf++;
@@ -118,31 +122,34 @@ class StatisticsHandlerInterface {
 	}
 
 
-	/**
+ 	/**
      * Return the discrimination value of the data. Discrimination is the difference between 
-     * the mean value of the top 27% and the mean value of the bottom 27% calculated separately. In the case of test results.
-     * the discrimination ranges from 0.0 to 1.0
-	 * @return integer The discrimination value of the data.
+     * the mean value of the top 27% and the mean value of the bottom 27% calculated separately. In the case of test results
+     * the discrimination ranges from 0 to 100, so divide the result by 100 before printing.
+	 * @return float The discrimination value of the data.
 	 * @access public
 	 */
 	function getDiscrimination() { 
 		if(!isset($this->_discrimination)){
-			if(!isset($this->_sortedData))
-				$this->_sortedData = sort($this->_data);
-			
+			if(!isset($this->_sortedData)){
+				$this->_sortedData = $this->_data;
+				sort($this->_sortedData);
+			}
+
 			$sum1 = 0;
 			$sum2 = 0;
 
 			$dataSize = count($this->_data);
-			$size = round(dataSize*27.0/100);
+			$size = round($dataSize*27.0/100);
 
-			for($ii=0;$ii<=size;$ii+){
+			for($ii=0;$ii<$size;$ii++){
 				$sum1 += $this->_sortedData[$ii];
 				$sum2 += $this->_sortedData[$dataSize-$ii-1];
 			}
-
-			$sum1 /= $sum2 /= $size*1.0;
 			
+			$sum1 /= $size;	
+			$sum2 /= $size;	
+
 			$this->_discrimination = round($sum2 - $sum1,3);
 		}
 
@@ -154,13 +161,14 @@ class StatisticsHandlerInterface {
      * The function will take the results of the original, calculate the top 27% of them and see how these compare
      * to the results of the second data. The same will be done for the bottom 27%, after which the results will be substracted.
      * This should be used if the original data is the overall results of the test and the second set is the results of one particular question.
-     * @param object $secondData The data to get the discrimination from. The values should be in the same order as the original data. 
+     * @param object $secondData The data to get the discrimination from. It should have the same number of elements as the original data and 
+     * The values should be in the same order as the original.
 	 * Clearly $secondData should either be an array or also have next() and  hasNext().
-	 * @return integer The discrimination of the second set in accordance with the first.
+	 * @return float The discrimination of the second set in accordance with the first.
 	 * @access public
 	 */
 	function getSecondaryDiscrimination(& $secondData) { 
-		if (isarray($secondData))
+		if (is_array($secondData))
 			$second = $secondData;
 		else {
 			$second = array();
@@ -176,19 +184,18 @@ class StatisticsHandlerInterface {
 		$sum2 = 0;
 
 		$dataSize = count($second);
-		$size = round(dataSize*27.0/100);
+		$size = round($dataSize*27.0/100);
 
-		for($ii=0;$ii<=size;$ii+){
+		for($ii=0;$ii<$size;$ii++){
 			$sum1 += $second[$ii];
 			$sum2 += $second[$dataSize-$ii-1];
 		}
 
-		$sum1 /= $sum2 /= $size;	
-			
+		$sum1 /= $size;
+		$sum2 /= $size;	
+
 		return round($sum2 - $sum1,3);
 	}
-
-	
 }
 
 
--- utilities/StatisticsHandler.interface.php
+++ utilities/StatisticsHandler.interface.php
@@ -3,7 +3,7 @@
 /**
  * An interface to calculate various statistical information.
  *
- * @version $Id: StatisticsHandler.interface.php,v 1.1 2003/07/10 22:12:08 movsjani Exp $
+ * @version $Id: StatisticsHandler.interface.php,v 1.2 2003/07/10 23:04:47 movsjani Exp $
  * @package harmoni.utilities
  * @copyright 2003 
  */
@@ -18,37 +18,37 @@ class StatisticsHandlerInterface {
 
 	/**
      * Return the mean value of the data.
-	 * @return integer The mean value of the data.
+	 * @return float The mean value of the data.
 	 * @access public
 	 */
 	function getMean() { die ("Method <b>".__FUNCTION__."()</b> declared in interface<b> ".__CLASS__."</b> has not been overloaded in a child class."); }
 
 	/**
      * Return the median value of the data.
-	 * @return integer The median value of the data.
+	 * @return float The median value of the data.
 	 * @access public
 	 */
 	function getMedian() { die ("Method <b>".__FUNCTION__."()</b> declared in interface<b> ".__CLASS__."</b> has not been overloaded in a child class."); }
 
 	/**
      * Return the standard deviation of the data.
-	 * @return integer The standard deviation of  the data.
+	 * @return float The standard deviation of  the data.
 	 * @access public
 	 */
 	function getStandardDeviation() { die ("Method <b>".__FUNCTION__."()</b> declared in interface<b> ".__CLASS__."</b> has not been overloaded in a child class."); }
 
 	/**
      * Return the modal (most frequent value) of the data.
-	 * @return integer The modal (most frequent value) of the data.
+	 * @return float The modal (most frequent value) of the data.
 	 * @access public
 	 */
 	function getModal() { die ("Method <b>".__FUNCTION__."()</b> declared in interface<b> ".__CLASS__."</b> has not been overloaded in a child class."); }
 
-	/**
+ 	/**
      * Return the discrimination value of the data. Discrimination is the difference between 
      * the mean value of the top 27% and the mean value of the bottom 27% calculated separately. In the case of test results
-     * the discrimination ranges from 0.0 to 1.0
-	 * @return integer The discrimination value of the data.
+     * the discrimination ranges from 0 to 100, so divide the result by 100 before printing.
+	 * @return float The discrimination value of the data.
 	 * @access public
 	 */
 	function getDiscrimination() { die ("Method <b>".__FUNCTION__."()</b> declared in interface<b> ".__CLASS__."</b> has not been overloaded in a child class."); }
@@ -58,9 +58,10 @@ class StatisticsHandlerInterface {
      * The function will take the results of the original, calculate who got the top 27% of them and see how these compare
      * to the results of the second data. The same will be done for the bottom 27%, after which the results will be substracted.
      * This should be used if the original data is the overall results of the test and the second set is the results of one particular question.
-     * @param object $secondData The data to get the discrimination from. The values should be in the same order as the original data 
-     * (the internals of this class do not alter that order). Of course, $secondData should also have next(), hasNext(), and getSize().
-	 * @return integer The discrimination of the second set in accordance with the first.
+     * @param object $secondData The data to get the discrimination from. It should have the same number of elements as the original data and 
+     * The values should be in the same order as the original.
+	 * Clearly $secondData should either be an array or also have next() and  hasNext().
+	 * @return float The discrimination of the second set in accordance with the first.
 	 * @access public
 	 */
 	function getSecondaryDiscrimination(& $secondData) { die ("Method <b>".__FUNCTION__."()</b> declared in interface<b> ".__CLASS__."</b> has not been overloaded in a child class."); }
