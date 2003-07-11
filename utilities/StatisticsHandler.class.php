fe76ee9eff16cae1e7d4172cd5317c9ef7ad3c16
movsjani <movsjani>
2003-07-11 16:14:02 +0000

    tabs

--- utilities/StatisticsHandler.class.php
+++ utilities/StatisticsHandler.class.php
@@ -4,7 +4,7 @@ require_once(HARMONI.'utilities/StatisticsHandler.interface.php');
 /**
  * An interface to calculate various statistical information.
  *
- * @version $Id: StatisticsHandler.class.php,v 1.2 2003/07/10 23:04:47 movsjani Exp $
+ * @version $Id: StatisticsHandler.class.php,v 1.3 2003/07/11 16:14:02 movsjani Exp $
  * @package harmoni.utilities
  * @copyright 2003 
  */
@@ -16,11 +16,12 @@ class StatisticsHandler extends StatisticsHandlerInterface{
 
 	var $_mean, $_median, $_discrimination, $_standardDeviation;
 
- 	/**
-	 * Create a handler with given data.
-	 * @param object $data The object, which contains all the data. It can either be an array or should be provided with methods: next() and getNext().
-	 * @access public
-	 */	
+   /**
+    * Create a Add a handler with given information.
+    * @param object $data The object, which contains all the data. It should be provided with methods: next(), getNext() and getSize().
+    * @access public
+    */	
+
 	function StatisticsHandler(& $data) { 
 		if(is_array($data))
 			$this->_data = $data;
@@ -33,11 +34,11 @@ class StatisticsHandler extends StatisticsHandlerInterface{
 		}
 	}
 
-	/**
-     * Return the mean value of the data.
-	 * @return float The mean value of the data.
-	 * @access public
-	 */
+   /**
+    * Return the mean value of the data.
+    * @return float The mean value of the data.
+    * @access public
+    */
 	function getMean() { 
 		if(!isset($this->_mean)){
 			$sum = 0;
@@ -52,11 +53,11 @@ class StatisticsHandler extends StatisticsHandlerInterface{
 		return $this->_mean;
 	}
 
-	/**
-     * Return the median value of the data.
-	 * @return float The median value of the data.
-	 * @access public
-	 */
+   /**
+    * Return the median value of the data.
+    * @return float The median value of the data.
+    * @access public
+    */
 	function getMedian() { 
 		if(!isset($this->_median)){
 			if(!isset($this->_sortedData)){
@@ -71,12 +72,11 @@ class StatisticsHandler extends StatisticsHandlerInterface{
 	}
 
 
-
-	/**
-     * Return the standard deviation of the data.
-	 * @return float The standard deviation of  the data.
-	 * @access public
-	 */
+   /**
+    * Return the standard deviation of the data.
+    * @return float The standard deviation of  the data.
+    * @access public
+    */
 	function getStandardDeviation() { 
 		if(!isset($this->_standardDeviation)) {
 			$mean = $this->getMean();
@@ -92,11 +92,11 @@ class StatisticsHandler extends StatisticsHandlerInterface{
 		return $this->_standardDeviation;
 	}
 
-	/**
-     * Return the modal (most frequent value) of the data.
-	 * @return float The modal (most frequent value) of the data.
-	 * @access public
-	 */
+   /**
+    * Return the modal (most frequent value) of the data.
+    * @return float The modal (most frequent value) of the data.
+    * @access public
+    */
 	function getModal() { 
 		if (!isset($this->_modal)) {
 			$cf=0; $mf=0; $current = '';
@@ -122,13 +122,13 @@ class StatisticsHandler extends StatisticsHandlerInterface{
 	}
 
 
- 	/**
-     * Return the discrimination value of the data. Discrimination is the difference between 
-     * the mean value of the top 27% and the mean value of the bottom 27% calculated separately. In the case of test results
-     * the discrimination ranges from 0 to 100, so divide the result by 100 before printing.
-	 * @return float The discrimination value of the data.
-	 * @access public
-	 */
+   /**
+    * Return the discrimination value of the data. Discrimination is the difference between 
+    * the mean value of the top 27% and the mean value of the bottom 27% calculated separately. In the case of test results
+    * the discrimination ranges from 0 to 100, so divide the result by 100 before printing.
+    * @return float The discrimination value of the data.
+    * @access public
+    */
 	function getDiscrimination() { 
 		if(!isset($this->_discrimination)){
 			if(!isset($this->_sortedData)){
@@ -156,17 +156,17 @@ class StatisticsHandler extends StatisticsHandlerInterface{
 		return $this->_discrimination;
 	}
 
-    /**
-     * Return the Discrimination of a different set in accordance to the base set.  
-     * The function will take the results of the original, calculate the top 27% of them and see how these compare
-     * to the results of the second data. The same will be done for the bottom 27%, after which the results will be substracted.
-     * This should be used if the original data is the overall results of the test and the second set is the results of one particular question.
-     * @param object $secondData The data to get the discrimination from. It should have the same number of elements as the original data and 
-     * The values should be in the same order as the original.
-	 * Clearly $secondData should either be an array or also have next() and  hasNext().
-	 * @return float The discrimination of the second set in accordance with the first.
-	 * @access public
-	 */
+   /**
+    * Return the Discrimination of a different set in accordance to the base set.  
+    * The function will take the results of the original, calculate who got the top 27% of them and see how these compare
+    * to the results of the second data. The same will be done for the bottom 27%, after which the results will be substracted.
+    * This should be used if the original data is the overall results of the test and the second set is the results of one particular question.
+    * @param object $secondData The data to get the discrimination from. It should have the same number of elements as the original data and 
+    * The values should be in the same order as the original.
+    * Clearly $secondData should either be an array or also have next() and  hasNext().
+    * @return float The discrimination of the second set in accordance with the first.
+    * @access public
+    */
 	function getSecondaryDiscrimination(& $secondData) { 
 		if (is_array($secondData))
 			$second = $secondData;
