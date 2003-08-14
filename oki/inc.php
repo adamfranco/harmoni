8427e72ae3b0a06277fbc970a8213ac14f94dd21
gabeschine <gabeschine>
2003-08-14 17:02:14 +0000

    created POC for Java-OKI integration

--- oki/inc.php
+++ oki/inc.php
@@ -1,4 +1,5 @@
 <?php // :: PHP OKI include all interfaces :: //
+require_once(OKI."root/rootAPI.interface.php");
 
 require_once(OKI."assessment/assessmentAPI.interface.php");
 require_once(OKI."authentication/AuthenticationApi.interface.php");
@@ -12,7 +13,6 @@ require_once(OKI."filing/filingAPI.interface.php");
 require_once(OKI."grading/gradingAPI.interface.php");
 require_once(OKI."hierarchy/hierarchyApi.interface.php");
 require_once(OKI."logging/loggingApi.interface.php");
-require_once(OKI."root/rootAPI.interface.php");
 require_once(OKI."shared/sharedAPI.interface.php");
 require_once(OKI."sql/sqlApi.interface.php");
 // :: done :: //
