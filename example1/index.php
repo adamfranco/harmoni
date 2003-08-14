ca9e42d5074ae817bc1173756ee3632d1fbeb241
gabeschine <gabeschine>
2003-08-14 19:42:58 +0000

    *** empty log message ***

--- example1/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once ("harmoni/Harmoni.inc");
-	$harmoni = new Harmoni ();
-	$harmoni->execute ();
-?>
--- simple_test/simple_web_test.php
+++
@@ -1,4 +0,0 @@
-<?php
-    // $Id: simple_web_test.php,v 1.1 2003/06/16 20:01:08 dobomode Exp $
-    
-?>
