a545ba3ff73c5aacbb05ffe357d30292873a043a
gabeschine <gabeschine>
2003-08-14 19:36:39 +0000

    moved files to core

--- oki/inc.php
+++ oki/inc.php
@@ -1,6 +1,8 @@
 <?php // :: PHP OKI include all interfaces :: //
 require_once(OKI."root/rootAPI.interface.php");
 
+require_once(OKI."root/rootAPI.interface.php");
+
 require_once(OKI."assessment/assessmentAPI.interface.php");
 require_once(OKI."authentication/AuthenticationApi.interface.php");
 require_once(OKI."authorization/AuthorizationAPI.interface.php");
@@ -16,4 +18,4 @@ require_once(OKI."logging/loggingApi.interface.php");
 require_once(OKI."shared/sharedAPI.interface.php");
 require_once(OKI."sql/sqlApi.interface.php");
 // :: done :: //
-?> // end PHP
+?>
