3891fa6ded985f65c83da60b578156e72b7d26ef
adamfranco <adamfranco>
2003-10-01 20:51:57 +0000

    implimenting Hierarchy using Richard Heyes' Tree class

--- core/oki/hierarchy/HarmoniHierarchy.class.php
+++ core/oki/hierarchy/HarmoniHierarchy.class.php
@@ -23,9 +23,9 @@ class HarmoniHierarchy
 	var $_displayName;
 	
 	/**
-	 * @var array $_rootNodes An array of root nodes in this hierarchy.
+	 * @var object Tree $_tree A tree object.
 	 */
-	var $_rootNodes = array();
+	var $_tree = new Tree();
 	
 	// public osid.shared.Id & getId();
 	function & getId() {
@@ -64,10 +64,36 @@ class HarmoniHierarchy
 	}
 
 	// public Node & createRootNode();
-	function & createRootNode() {
-		die ("Method <b>".__FUNCTION__."()</b> declared in interface <b> ".__CLASS__."</b> has not been overloaded in a child class.");
+	function & createRootNode(& $nodeId, & $type, $displayName, $description) {
+		// return a node with itself as the parent.
+		return createNode($nodeId, $nodeId, $type, $displayName, $description);
 	}
 
+	// public Node & createNode();
+	function & createNode(& $nodeId, & $parentId, & $type, $displayName, $description) {
+		// Check the arguments
+		ArgumentValidator::validate($nodeId, new ExtendsValidatorRule("Id"));
+		ArgumentValidator::validate($parentId, new ExtendsValidatorRule("Id"));
+		ArgumentValidator::validate($type, new ExtendsValidatorRule("Type"));
+		ArgumentValidator::validate($displayName, new StringValidatorRule);
+		ArgumentValidator::validate($description, new StringValidatorRule);
+		
+		$nodeIdString = $nodeId->getIdString();
+		if ($parentId->isEqual($nodeId)) { // if this is a root node (its parent is itsself)
+			// Richard Heyes' tree implimentation uses 0 as a parent node id to indicate
+			// a root node.
+			$parentIdString = 0;
+		} else {	// if this is not a root node
+			$parentIdString = $parentId->getIdString();
+			// Check that the parent exists
+			if (!$this->_tree->nodeExists($parentIdString))
+				throwError(new Error(UNKNOWN_PARENT_NODE, "Hierarchy", 1));
+		}
+		
+		$node =& new HaromoniNode($this->_tree, $type, $displayName, $description);
+		$treeNodeId = $this->_tree->addNode($node, $parentIdString);
+	}
+	
 	// public osid.shared.Id & nodeId();
 	function & nodeId() {
 		die ("Method <b>".__FUNCTION__."()</b> declared in interface <b> ".__CLASS__."</b> has not been overloaded in a child class.");
@@ -85,11 +111,6 @@ class HarmoniHierarchy
 
 	// public String description();
 	function description() {
-		die ("Method <b>".__FUNCTION__."()</b> declared in interface <b> ".__CLASS__."</b> has not been overloaded in a child class.");
-	}
-
-	// public Node & createNode();
-	function & createNode() {
 		die ("Method <b>".__FUNCTION__."()</b> declared in interface <b> ".__CLASS__."</b> has not been overloaded in a child class.");
 	}
 
