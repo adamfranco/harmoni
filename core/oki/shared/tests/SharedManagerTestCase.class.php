f481f6a5fe91a8cb521f5fabcc65eb7c93fce3a2
adamfranco <adamfranco>
2003-10-17 15:52:56 +0000

    Adding Shared tests

---
+++ core/oki/shared/tests/SharedManagerTestCase.class.php
@@ -0,0 +1,126 @@
+<?php
+
+require_once(HARMONI.'/oki/hierarchy/HarmoniHierarchyManager.class.php');
+require_once(HARMONI.'/oki/shared/HarmoniTestId.class.php');
+require_once(HARMONI.'/oki/hierarchy/tests/TestNodeType.class.php');
+
+/**
+ * A single unit test case. This class is intended to test one particular
+ * class. Replace 'testedclass.php' below with the class you would like to
+ * test.
+ *
+ * @version $Id: SharedManagerTestCase.class.php,v 1.1 2003/10/17 15:52:56 adamfranco Exp $
+ * @package concerto.tests.api.metadata
+ * @copyright 2003
+ **/
+
+    class HarmoniHierarcyManagerTestCase extends UnitTestCase {
+	
+		var $manager;
+
+        /**
+         *    Sets up unit test wide variables at the start
+         *    of each test method.
+         *    @public
+         */
+        function setUp() {
+        	print "<pre>";
+        	
+        	$this->manager =& new HarmoniHierarchyManager;
+        	$nodeType =& new TestNodeType;
+			$nodeTypes = array ($nodeType);
+			$this->manager->createHierarchy(FALSE, "A test Hierarchy", "Hierarchy1", $nodeTypes, FALSE);
+			$this->manager->createHierarchy(FALSE, "Another test Hierarchy", "Hierarchy2", $nodeTypes, FALSE);
+        }
+		
+        /**
+         *    Clears the data set in the setUp() method call.
+         *    @public
+         */
+        function tearDown() {
+			// perhaps, unset $obj here
+			unset($this->manager);
+			print "</pre>";
+        }
+
+		//--------------the tests ----------------------
+
+		function test_constructor() {
+			$manager =& new HarmoniHierarchyManager;
+			$this->assertTrue(is_object($manager));
+		}
+		
+		function test_hierarchy_creation() {
+			$manager =& new HarmoniHierarchyManager;
+			$nodeType =& new TestNodeType;
+			$nodeTypes = array ($nodeType);
+			$manager->createHierarchy(FALSE, "A test Hierarchy", "Hierarchy1", $nodeTypes, FALSE);
+			$hierarchies =& $manager->getHierarchies();
+			$hierarchy =& $hierarchies->next();
+			
+			$this->assertIsA($hierarchy, "HarmoniHierarchy");
+			
+			$manager->createHierarchy(FALSE, "Another test Hierarchy", "Hierarchy2", $nodeTypes, FALSE);
+			$hierarchies =& $manager->getHierarchies();
+			$hierarchies->next();
+			$hierarchy =& $hierarchies->next();
+			
+//			print_r ($manager);
+			$this->assertIsA($hierarchy, "HarmoniHierarchy");
+		}
+		
+		function test_get_hierarchies() {
+			$manager =& $this->manager;
+			
+			$nodeType =& new TestNodeType;
+			$nodeTypes = array ($nodeType);
+			$thirdHierarchy =& $manager->createHierarchy(FALSE, "Yet another test Hierarchy", "Hierarchy3", $nodeTypes, FALSE);
+			
+			$hierarchies =& $manager->getHierarchies();
+			$count = 0;
+			while ($hierarchies->hasNext()) {
+				$count++;
+				$hierarchy =& $hierarchies->next();
+			}
+			$this->assertEqual($count, 3);
+			$this->assertReference($hierarchy, $thirdHierarchy);
+		}
+		
+		function test_get_hierarchy() {
+			$manager =& $this->manager;
+			
+			$nodeType =& new TestNodeType;
+			$nodeTypes = array ($nodeType);
+			$thirdHierarchy =& $manager->createHierarchy(FALSE, "Yet another test Hierarchy", "Hierarchy3", $nodeTypes, FALSE);
+			$thirdHierarchyId = $thirdHierarchy->getId();
+			$hierarchy =& $manager->getHierarchy($thirdHierarchyId);
+			$this->assertReference($hierarchy, $thirdHierarchy);
+		}
+		
+		function test_delete_hierarchy() {
+			$manager =& $this->manager;
+			
+						$nodeType =& new TestNodeType;
+			$nodeTypes = array ($nodeType);
+			$thirdHierarchy =& $manager->createHierarchy(FALSE, "Yet another test Hierarchy", "Hierarchy3", $nodeTypes, FALSE);
+			$thirdHierarchyId = $thirdHierarchy->getId();
+			
+			$hierarchies =& $manager->getHierarchies();
+			$count = 0;
+			while ($hierarchies->hasNext()) {
+				$count++;
+				$hierarchies->next();
+			}
+			$this->assertEqual($count, 3);
+			
+			$manager->deleteHierarchy($thirdHierarchyId);
+			
+			$hierarchies =& $manager->getHierarchies();
+			$count = 0;
+			while ($hierarchies->hasNext()) {
+				$count++;
+				$hierarchies->next();
+			}
+			$this->assertEqual($count, 2);
+		}
+	}
---
+++ core/oki/shared/tests/test.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * A group test template using the SimpleTest unit testing package.
+ * Just add the UnitTestCase files below using addTestFile().
+ *
+ * @version $Id: test.php,v 1.1 2003/10/17 15:52:56 adamfranco Exp $
+ * @package concerto.tests.api.metadata
+ * @copyright 2003 
+ **/
+ 
+	require_once(dirname(__FILE__)."/../../../../harmoni.inc.php");
+        
+    require_once(SIMPLE_TEST . 'simple_unit.php');
+    require_once(SIMPLE_TEST . 'dobo_simple_html_test.php');
+
+/* 	if (!defined('CONCERTODBID')) { */
+/* 		require_once(CONCERTO.'/tests/dbconnect.inc.php'); */
+/* 	} */
+
+	require_once(HARMONI."errorHandler/ErrorHandler.class.php");
+	$errorHandler =& Services::requireService("ErrorHandler","ErrorHandler");
+	$errorHandler->setDebugMode(TRUE);
+	
+	
+    $test =& new GroupTest('Shared Tests');
+    $test->addTestFile(HARMONI.'/oki/shared/tests/DatabaseIdTestCase.class.php');
+
+    $test->attachObserver(new DoboTestHtmlDisplay());
+    $test->run();
+	
+//	$errorHandler->printErrors(HIGH_DETAIL);
+//	print "<pre>";
+//	print_r($errorHandler);
+?>
