21a2b2cdb0e5c28e0945c239c229fe16ef6f2fa8
adamfranco <adamfranco>
2004-08-02 20:46:34 +0000

    Add SQL definition for sets.

---
+++ SQL/sets/MySQL_sets.sql
@@ -0,0 +1,6 @@
+CREATE TABLE sets (
+  id int(11) NOT NULL default '0',
+  item_id int(11) NOT NULL default '0',
+  order int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (item_id,id)
+) TYPE=MyISAM;
