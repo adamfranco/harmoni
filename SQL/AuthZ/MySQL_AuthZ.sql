be1297c3ae4af85ec5bbc98c11aa65ffccc7e8ae
adamfranco <adamfranco>
2004-08-06 17:24:39 +0000

    Adding forgotten SQL tables from a dump of dobo's db. These are untested.

---
+++ SQL/AuthZ/MySQL_AuthZ.sql
@@ -0,0 +1,28 @@
+CREATE TABLE az_authorization (
+  authorization_id int(10) unsigned NOT NULL default '0',
+  fk_agent int(10) unsigned NOT NULL default '0',
+  fk_function int(10) unsigned NOT NULL default '0',
+  fk_qualifier int(10) unsigned NOT NULL default '0',
+  authorization_effective_date datetime default NULL,
+  authorization_expiration_date datetime default NULL,
+  PRIMARY KEY  (authorization_id),
+  UNIQUE KEY uniq (fk_agent,fk_function,fk_qualifier),
+  KEY fk_agent (fk_agent),
+  KEY fk_function (fk_function),
+  KEY fk_qualifier (fk_qualifier),
+  KEY authorization_effective_date (authorization_effective_date),
+  KEY authorization_expiration_date (authorization_expiration_date)
+) TYPE=MyISAM;
+
+
+CREATE TABLE az_function (
+  function_id int(10) unsigned NOT NULL default '0',
+  function_reference_name varchar(255) NOT NULL default '',
+  function_description text NOT NULL,
+  fk_qualifier_hierarchy int(10) unsigned NOT NULL default '0',
+  fk_type int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (function_id),
+  KEY function_display_name (function_reference_name),
+  KEY fk_qualifier_hierarchy_id (fk_qualifier_hierarchy),
+  KEY fk_type (fk_type)
+) TYPE=MyISAM;
