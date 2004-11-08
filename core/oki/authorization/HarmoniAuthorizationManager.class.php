1e02176cf20696d4be782ad3b2f7d52b4ed8094c
adamfranco <adamfranco>
2004-11-08 16:45:02 +0000

    Making it fullfill the Services.interface.php

--- core/oki/authorization/HarmoniAuthorizationManager.class.php
+++ core/oki/authorization/HarmoniAuthorizationManager.class.php
@@ -11,7 +11,7 @@ require_once(HARMONI.'oki/shared/HarmoniIdIterator.class.php');
 /**
  * AuthorizationManager allows an application to create Authorizations, get Authorizations given selection criterias, ask questions of Authorization such as what Agent can do a Function in a Qualifier context, etc.<p><p>The primary objects in Authorization are Authorization, Function, Agent, and Qualifier. There are also Function and Qualifier types that are understood by the implementation.<p><p>Ids in Authorization are externally defined and their uniqueness is enforced by the implementation. <p><p>There are two methods to create Authorizations. One uses method uses Agent, Function, and Qualifier.  The other adds effective date and expiration date.  For the method without the dates, the effective date is today and there is no expiration date.  <p>SID Version: 1.0 rc6 <p>Licensed under the {@link SidLicense MIT O.K.I&#46; SID Definition License}.
  * @access public
- * @version $Id: HarmoniAuthorizationManager.class.php,v 1.10 2004/08/26 15:10:33 adamfranco Exp $
+ * @version $Id: HarmoniAuthorizationManager.class.php,v 1.11 2004/11/08 16:45:02 adamfranco Exp $
  * @author Middlebury College, ETS
  * @copyright 2003 Middlebury College, ETS
  * @package harmoni.osid.authorization
@@ -666,6 +666,27 @@ class HarmoniAuthorizationManager extends AuthorizationManager {
 	function &getQualifierHierarchies() {
 		$hierarchyManager =& Services::requireService("Hierarchy", true);
 		return $hierarchyManager->getHierarchies();
+	}
+
+	/**
+	 * The start function is called when a service is created. Services may
+	 * want to do pre-processing setup before any users are allowed access to
+	 * them.
+	 * @access public
+	 * @return void
+	 **/
+	function start() {
+	}
+	
+	/**
+	 * The stop function is called when a Harmoni service object is being 
+	 * destroyed.
+	 * Services may want to do post-processing such as content output or 
+	 * committing changes to a database, etc.
+	 * @access public
+	 * @return void
+	 **/
+	function stop() {
 	}
 
 }
