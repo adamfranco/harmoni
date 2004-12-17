be8b6309f6476ea59fbce91ed4b59ac08fe3f9cc
adamfranco <adamfranco>
2004-12-17 15:22:21 +0000

    Was passing Id objects instead of the required Id strings.

--- core/oki/authorization/HarmoniAuthorizationManager.class.php
+++ core/oki/authorization/HarmoniAuthorizationManager.class.php
@@ -11,7 +11,7 @@ require_once(HARMONI.'oki/shared/HarmoniIdIterator.class.php');
 /**
  * AuthorizationManager allows an application to create Authorizations, get Authorizations given selection criterias, ask questions of Authorization such as what Agent can do a Function in a Qualifier context, etc.<p><p>The primary objects in Authorization are Authorization, Function, Agent, and Qualifier. There are also Function and Qualifier types that are understood by the implementation.<p><p>Ids in Authorization are externally defined and their uniqueness is enforced by the implementation. <p><p>There are two methods to create Authorizations. One uses method uses Agent, Function, and Qualifier.  The other adds effective date and expiration date.  For the method without the dates, the effective date is today and there is no expiration date.  <p>SID Version: 1.0 rc6 <p>Licensed under the {@link SidLicense MIT O.K.I&#46; SID Definition License}.
  * @access public
- * @version $Id: HarmoniAuthorizationManager.class.php,v 1.22 2004/12/13 19:47:14 adamfranco Exp $
+ * @version $Id: HarmoniAuthorizationManager.class.php,v 1.23 2004/12/17 15:22:21 adamfranco Exp $
  * @author Middlebury College, ETS
  * @copyright 2003 Middlebury College, ETS
  * @package harmoni.osid.authorization
@@ -417,7 +417,7 @@ class HarmoniAuthorizationManager extends AuthorizationManager {
 		$authentication =& Services::requireService("AuthN", true);
 		$userId =& $authentication->getUserId(new HarmoniAuthenticationType());
 		
-		$authorizations =& $this->_cache->getAZs($userId,
+		$authorizations =& $this->_cache->getAZs($userId->getIdString(),
 												 $functionId->getIdString(),
 												 $qualifierId->getIdString(),
 												 null, 
@@ -448,7 +448,7 @@ class HarmoniAuthorizationManager extends AuthorizationManager {
 		$authentication =& Services::requireService("AuthN", true);
 		$userId =& $authentication->getUserId(new HarmoniAuthenticationType());
 		
-		$authorizations =& $this->_cache->getAZs($userId,
+		$authorizations =& $this->_cache->getAZs($userId->getIdString(),
 												 null,
 												 $qualifierId->getIdString(),
 												 $functionType, 
@@ -479,7 +479,7 @@ class HarmoniAuthorizationManager extends AuthorizationManager {
 		$authentication =& Services::requireService("AuthN", true);
 		$userId =& $authentication->getUserId(new HarmoniAuthenticationType());
 		
-		$authorizations =& $this->_cache->getAZs($userId,
+		$authorizations =& $this->_cache->getAZs($userId->getIdString(),
 									 $functionId->getIdString(),
 									 $qualifierId->getIdString(),
 									 null, 
@@ -511,7 +511,7 @@ class HarmoniAuthorizationManager extends AuthorizationManager {
 		$authentication =& Services::requireService("AuthN", true);
 		$userId =& $authentication->getUserId(new HarmoniAuthenticationType());
 		
-		$authorizations =& $this->_cache->getAZs($userId,
+		$authorizations =& $this->_cache->getAZs($userId->getIdString(),
 									 null,
 									 $qualifierId->getIdString(),
 									 $functionType, 
