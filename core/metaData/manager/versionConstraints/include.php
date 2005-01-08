f14aab3c0a840863f325175c1a05a297728290b4
gabeschine <gabeschine>
2005-01-08 02:15:53 +0000

    old, removed

--- core/metaData/manager/versionConstraints/include.php
+++
@@ -1,7 +0,0 @@
-<?
-
-require_once HARMONI."metaData/manager/versionConstraints/DateVersionConstraint.class.php";
-require_once HARMONI."metaData/manager/versionConstraints/NumberVersionConstraint.class.php";
-require_once HARMONI."metaData/manager/versionConstraints/PruneAllVersionConstraint.class.php";
-
-?>
