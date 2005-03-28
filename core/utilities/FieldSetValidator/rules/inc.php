2d1ac67531856c3f5e7a67a35e8610a630bdde5e
adamfranco <adamfranco>
2005-03-28 23:03:52 +0000

    now doesn't include ._XxxxxXxxx files

--- core/utilities/FieldSetValidator/rules/inc.php
+++ core/utilities/FieldSetValidator/rules/inc.php
@@ -8,11 +8,12 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: inc.php,v 1.3 2005/02/04 15:59:13 adamfranco Exp $
+ * @version $Id: inc.php,v 1.4 2005/03/28 23:03:52 adamfranco Exp $
  */
 $_dh = opendir(dirname(__FILE__));
 while ($_f = readdir($_dh)) {
-	if (ereg(".class.php$", $_f)) require_once(dirname(__FILE__)."/$_f");
+	if (ereg("^[^\.].+\.class\.php$", $_f))
+		require_once(dirname(__FILE__)."/$_f");
 }
 unset($_f,$_dh);
 
