a451420121a1438c2e06abf3cd115fb12b91ad40
adamfranco <adamfranco>
2005-04-01 21:56:04 +0000

    ImageProcessingManager now uses assignConfiguration() instead of constructor args.

--- core/ImageProcessor/ImageProcessor.class.php
+++ core/ImageProcessor/ImageProcessor.class.php
@@ -11,7 +11,7 @@ require_once(dirname(__FILE__)."/ImageMagickProcessor.class.php");
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: ImageProcessor.class.php,v 1.5 2005/03/29 19:44:11 adamfranco Exp $
+ * @version $Id: ImageProcessor.class.php,v 1.6 2005/04/01 21:56:04 adamfranco Exp $
  */
 
 class ImageProcessor {
@@ -35,32 +35,102 @@ class ImageProcessor {
 	 * @access public
 	 * @since 10/22/04
 	 */
-	function ImageProcessor ($thumbnailFormat, $useGD, $gdFormats, 
-		$useImageMagick = FALSE, $ImageMagickPath = "/usr/bin", 
-		$ImageMagickTempDir = "/tmp", $ImageMagickFormats = array()) 
-	{
-		ArgumentValidator::validate($thumbnailFormat, StringValidatorRule::getRule());
-		ArgumentValidator::validate($useGD, BooleanValidatorRule::getRule());
-		ArgumentValidator::validate($gdFormats, ArrayValidatorRuleWithRule::getRule(StringValidatorRule::getRule()));
-		ArgumentValidator::validate($useImageMagick, BooleanValidatorRule::getRule());
-		ArgumentValidator::validate($ImageMagickPath, StringValidatorRule::getRule());
-		ArgumentValidator::validate($ImageMagickTempDir, StringValidatorRule::getRule());
-		ArgumentValidator::validate($ImageMagickFormats, ArrayValidatorRuleWithRule::getRule(StringValidatorRule::getRule()));
+	function ImageProcessor () {}
+	
+	/**
+	 * Assign the configuration of this Manager. Valid configuration options are as
+	 * follows:
+	 *
+	 *	thumbnail_format		string	(ex: 'image/jpeg')
+	 *
+	 *	use_gd					boolean
+	 I
+	 *	gd_formats				array of strings	(ex: array('image/jpeg', 'image/png'))
+	 *							An empty array indicates any/all supported formats.
+	 *
+	 *	use_imagemagick			boolean
+	 *
+	 *	imagemagick_path		string	(ex: 'usr/bin' OR '/usr/X11R6/bin')
+	 *
+	 *	imagemagick_temp_dir	string	(ex: '/tmp')
+	 *
+	 *	imagemagick_formats		array of strings	(ex: array('image/jpeg', 'image/png'))
+	 *							An empty array indicates any/all supported formats.
+	 * 
+	 * @param object Properties $configuration (original type: java.util.Properties)
+	 * 
+	 * @throws object OsidException An exception with one of the following
+	 *		   messages defined in org.osid.OsidException:	{@link
+	 *		   org.osid.OsidException#OPERATION_FAILED OPERATION_FAILED},
+	 *		   {@link org.osid.OsidException#PERMISSION_DENIED
+	 *		   PERMISSION_DENIED}, {@link
+	 *		   org.osid.OsidException#CONFIGURATION_ERROR
+	 *		   CONFIGURATION_ERROR}, {@link
+	 *		   org.osid.OsidException#UNIMPLEMENTED UNIMPLEMENTED}, {@link
+	 *		   org.osid.OsidException#NULL_ARGUMENT NULL_ARGUMENT}
+	 * 
+	 * @access public
+	 */
+	function assignConfiguration ( &$configuration ) { 
+		$this->_configuration =& $configuration;
 		
-		$this->_thumbnailFormat = $thumbnailFormat;
-		$this->_useGD = $useGD;
-		$this->_gdFormats = $gdFormats;
-		$this->_useImageMagick = $useImageMagick;
-		$this->_ImageMagickPath = $ImageMagickPath;
-		$this->_ImageMagickTempDir = $ImageMagickTempDir;
-		$this->_ImageMagickFormats = $ImageMagickFormats;
+		ArgumentValidator::validate($configuration->getProperty('thumbnail_format'), 
+			StringValidatorRule::getRule());
+		ArgumentValidator::validate($configuration->getProperty('use_gd'), 
+			BooleanValidatorRule::getRule());
+		ArgumentValidator::validate($configuration->getProperty('gd_formats'), 
+			ArrayValidatorRuleWithRule::getRule(StringValidatorRule::getRule()));
+		ArgumentValidator::validate($configuration->getProperty('use_imagemagick'), 
+			BooleanValidatorRule::getRule());
+		ArgumentValidator::validate($configuration->getProperty('imagemagick_path'), 
+			StringValidatorRule::getRule());
+		ArgumentValidator::validate($configuration->getProperty('imagemagick_temp_dir'), 
+			StringValidatorRule::getRule());
+		ArgumentValidator::validate($configuration->getProperty('imagemagick_formats'), 
+			ArrayValidatorRuleWithRule::getRule(StringValidatorRule::getRule()));
+		
+		$this->_thumbnailFormat = $configuration->getProperty('thumbnail_format');
+		$this->_useGD = $configuration->getProperty('use_gd');
+		$this->_gdFormats = $configuration->getProperty('gd_formats');
+		$this->_useImageMagick = $configuration->getProperty('use_imagemagick');
+		$this->_ImageMagickPath = $configuration->getProperty('imagemagick_path');
+		$this->_ImageMagickTempDir = $configuration->getProperty('imagemagick_temp_dir');
+		$this->_ImageMagickFormats = $configuration->getProperty('imagemagick_formats');
 		
 		if ($this->_useGD)
-			$this->_gdProcessor =& new GDProcessor($thumbnailFormat);
+			$this->_gdProcessor =& new GDProcessor($configuration->getProperty('thumbnail_format'));
 		if ($this->_useImageMagick)
 			$this->_ImageMagickProcessor =& new ImageMagickProcessor(
-						$thumbnailFormat, $this->_ImageMagickPath, 
+						$configuration->getProperty('thumbnail_format'), $this->_ImageMagickPath, 
 						$this->_ImageMagickTempDir);
+	}
+
+	/**
+	 * Return context of this OsidManager.
+	 *	
+	 * @return object OsidContext
+	 * 
+	 * @throws object OsidException 
+	 * 
+	 * @access public
+	 */
+	function &getOsidContext () { 
+		return $this->_osidContext;
+	} 
+
+	/**
+	 * Assign the context of this OsidManager.
+	 * 
+	 * @param object OsidContext $context
+	 * 
+	 * @throws object OsidException An exception with one of the following
+	 *		   messages defined in org.osid.OsidException:	{@link
+	 *		   org.osid.OsidException#NULL_ARGUMENT NULL_ARGUMENT}
+	 * 
+	 * @access public
+	 */
+	function assignOsidContext ( &$context ) { 
+		$this->_osidContext =& $context;
 	}
 	
 	/**
