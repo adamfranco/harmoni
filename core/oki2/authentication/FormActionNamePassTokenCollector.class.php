68670e321d456d051f1997ac97a474d2ce991bee
adamfranco <adamfranco>
2005-04-11 18:27:39 +0000

    Authentication now plays nicely with GET vars.

--- core/oki2/authentication/FormActionNamePassTokenCollector.class.php
+++ core/oki2/authentication/FormActionNamePassTokenCollector.class.php
@@ -5,7 +5,7 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: FormActionNamePassTokenCollector.class.php,v 1.1 2005/03/23 21:26:38 adamfranco Exp $
+ * @version $Id: FormActionNamePassTokenCollector.class.php,v 1.2 2005/04/11 18:27:39 adamfranco Exp $
  */ 
 
 require_once(dirname(__FILE__)."/BasicFormNamePassTokenCollector.class.php");
@@ -19,7 +19,7 @@ require_once(dirname(__FILE__)."/BasicFormNamePassTokenCollector.class.php");
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: FormActionNamePassTokenCollector.class.php,v 1.1 2005/03/23 21:26:38 adamfranco Exp $
+ * @version $Id: FormActionNamePassTokenCollector.class.php,v 1.2 2005/04/11 18:27:39 adamfranco Exp $
  */
 class FormActionNamePassTokenCollector
 	extends BasicFormNamePassTokenCollector
@@ -34,9 +34,14 @@ class FormActionNamePassTokenCollector
 	 * @since 3/22/05
 	 */
 	function FormActionNamePassTokenCollector ($url) {
+		if (count($_GET) && !ereg("\?", $url)) {
+			$url .= "?";
+			foreach ($_GET as $name => $value) {
+				$url .= "&".$name."=".$value;
+			}
+		}
+		
 		$this->_url = $url;
-// 		$this->_action = $action;
-// 		$this->_harmoni =& $GLOBALS['harmoni'];
 	}
 	/**
 	 * Prompt the user to supply their tokens
@@ -47,8 +52,6 @@ class FormActionNamePassTokenCollector
 	 */
 	function prompt () {
 		header("Location: ".$this->_url);
-// 		$this->_harmoni->ActionHandler->forward($this->_module, $this->_action);
-// 		exit;
 	}
 }
 
