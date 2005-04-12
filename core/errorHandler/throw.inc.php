46a392bd27eeaaef1ed2de6f24889ef2b2c9afba
adamfranco <adamfranco>
2005-04-12 18:09:32 +0000

    existance of a try() function was throwing parse errors on Win32 systems.

--- core/errorHandler/throw.inc.php
+++ core/errorHandler/throw.inc.php
@@ -8,7 +8,7 @@
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: throw.inc.php,v 1.9 2005/04/04 17:39:23 adamfranco Exp $
+ * @version $Id: throw.inc.php,v 1.10 2005/04/12 18:09:32 adamfranco Exp $
  */
 
 /**
@@ -20,40 +20,6 @@ function throwError(& $error) {
 
 	// throw the error
 	$errorHandler->addError($error);
-}
-
-/**
- * Try a function and catch any errors. If an error has occured,
- * the try will return FALSE. The error can then be accessed and
- * printed from the ErrorHandler if desired.
- *
- * @param mixed $operation The operation to run. Of the form:
- *  	try("$function($arg1, $arg2, etc)")
- * 	or
- *  	try("$object->$function($arg1, $arg2, etc)")
- * @return boolean TRUE if successful, FALSE if an error occurred.
- */
-function try ($operation) {
-	
-	die (UNIMPLEMENTED); 
-	
-	$errorHandler =& Services::getService("ErrorHandler");
-	
-	/* Several problems with implementing this function: 
-		- Debug mode (or equivalent without "die()" call) may allow unsafe
-		  execution to continue if the throwError() call was being used
-		  to stop exectution
-		  
-		- It seems that die() and exit() are the only functions that can 
-		  escape out of multiple functions. I can't figure out any way to
-		  get back to the try function and allow it to continue, while stopping
-		  the execution of the functions called withing try().
-		
-		- unclear how references may be handeled in the evaled string. Need
-		  to check on this.
-	*/
-	
-	eval ($operation);
 }
 
 /**
