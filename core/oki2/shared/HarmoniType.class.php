b917b5f0197d586e821001f9e522ab39d437a7c7
adamfranco <adamfranco>
2005-06-01 18:48:46 +0000

    stringToType and typeToString are now HarmoniType class methods.

--- core/oki2/shared/HarmoniType.class.php
+++ core/oki2/shared/HarmoniType.class.php
@@ -11,26 +11,71 @@ require_once(OKI2."/osid/shared/Type.php");
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: HarmoniType.class.php,v 1.8 2005/06/01 18:33:37 adamfranco Exp $
+ * @version $Id: HarmoniType.class.php,v 1.9 2005/06/01 18:48:46 adamfranco Exp $
  */
 
 class HarmoniType
 	extends Type
 {
 
+	/**
+	 * Convert an OKI Type to a delimited string
+	 * 
+	 * @param object Type $aType
+	 * @param string $glue
+	 * @return string
+	 * @access public
+	 * @since 6/1/05
+	 * @static
+	 */
+	 function typeToString(&$aType, $glue="::") {
+		ArgumentValidator::validate($aType, ExtendsValidatorRule::getRule("Type"));
+		return $aType->getDomain() . $glue . $aType->getAuthority() . $glue . $aType->getKeyword();
+	}
+	
+	/**
+	 * Convert a delimited string to an OKI Type
+	 * 
+	 * @param string $aString
+	 * @param string $glue
+	 * @return object Type
+	 * @access public
+	 * @since 6/1/05
+	 * @static
+	 */
+	function &stringToType($aString, $glue="::") {
+		ArgumentValidator::validate($string, StringValidatorRule::getRule());
+		
+		$parts = explode($glue, $string);
+		
+		return new Type($parts[0], $parts[1], $parts[2]);
+	}
 }
 
-function OKITypeToString(&$type, $glue="::") {
-	ArgumentValidator::validate($type, ExtendsValidatorRule::getRule("Type"));
-	return $type->getDomain() . $glue . $type->getAuthority() . $glue . $type->getKeyword();
+/**
+ * Convert an OKI Type to a delimited string
+ * 
+ * @param object Type $aType
+ * @param string $glue
+ * @return string
+ * @access public
+ * @since 6/1/05
+ */
+ function OKITypeToString(&$aType, $glue="::") {
+	return HarmoniType::typeToString($aType, $glue);
 }
 
-function &stringToOKIType($string, $glue="::") {
-	ArgumentValidator::validate($string, StringValidatorRule::getRule());
-	
-	$parts = explode($glue, $string);
-	
-	return new HarmoniType($parts[0], $parts[1], $parts[2]);
+/**
+ * Convert a delimited string to an OKI Type
+ * 
+ * @param string $aString
+ * @param string $glue
+ * @return object Type
+ * @access public
+ * @since 6/1/05
+ */
+function &stringToOKIType($aString, $glue="::") {
+	return HarmoniType::stringToType($aString, $glue);
 }
 
 ?>
