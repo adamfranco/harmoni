5cbb3bfbe41d9954d019f485f3e23102c2488540
cws-midd <cws-midd>
2005-07-21 14:18:49 +0000

    added boolean withValue

--- core/Primitives/Objects/Boolean.class.php
+++ core/Primitives/Objects/Boolean.class.php
@@ -11,7 +11,7 @@ require_once(dirname(__FILE__)."/../Objects/SObject.class.php");
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: Boolean.class.php,v 1.2 2005/07/14 16:23:21 adamfranco Exp $
+ * @version $Id: Boolean.class.php,v 1.3 2005/07/21 14:18:49 cws-midd Exp $
  */
 class Boolean 
 	extends SObject
@@ -19,6 +19,19 @@ class Boolean
 	
 	var $_bool;
 
+	/**
+	 * Instantiates a new Boolean object with the passed value.
+	 * @param string $value
+	 * @return ref object
+	 * @access public
+	 * @static
+	 */
+	function &withValue($value) {
+		$temp =& new Boolean($value);
+		return $temp;
+	}
+	
+	
 	function Boolean($value=true) {
 		$this->_bool = (bool) $value;
 	}
