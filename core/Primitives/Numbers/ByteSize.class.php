e05374544bc5fa8366038d3211a6fb4276d2c9fa
adamfranco <adamfranco>
2005-10-11 18:07:21 +0000

    Adding a ByteSize primitive.

---
+++ core/Primitives/Numbers/ByteSize.class.php
@@ -0,0 +1,187 @@
+<?
+
+/**
+ * @since 7/14/05
+ * @package harmoni.primitives.numbers
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: ByteSize.class.php,v 1.1 2005/10/11 18:07:21 adamfranco Exp $
+ */ 
+
+require_once(dirname(__FILE__)."/Integer.class.php");
+
+/**
+ * A representation of a Byte size. Provides easy conversion between B, KB, MB, etc
+ * as well as a pretty string reprentation
+ *
+ * @package harmoni.primitives.numbers
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: ByteSize.class.php,v 1.1 2005/10/11 18:07:21 adamfranco Exp $
+ */
+class ByteSize 
+	extends Integer
+{
+
+/*********************************************************
+ * Class Methods
+ *********************************************************/
+	
+	/**
+	 * Answer the string suffix for the desired muliple of 2^10 bytes
+	 * i.e. 0 -> B, 10 -> kB, 20 -> MB, 30 -> GB, etc.
+	 * 
+	 * @param integer $power A multiple of 10; Range, 0-80
+	 * @return string
+	 * @access public
+	 * @since 10/11/05
+	 */
+	function suffixForPower ($power) {
+		$multiple = intval($power/10);
+		if ($multiple < 0 || $multiple > 8)
+			throwError(new Error("Invalid power, $power. Valid values are multiples of ten, 0-80.",
+				"ByteSize", true));
+		
+		$suffixes = array("B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB");
+		return $suffixes[$multiple];
+	}
+	
+/*********************************************************
+ * Class Methods - Instance Creation
+ *********************************************************/
+
+	/**
+	 * Answer a new object with the value specified
+	 * 
+	 * @param integer $value Integer number of Bytes
+	 * @param optional string $class The class to instantiate. Do NOT use outside 
+	 *		of this package.
+	 * @return object Integer
+	 * @access public
+	 * @since 7/14/05
+	 */
+	function &withValue ( $value, $class = 'ByteSize') {
+		return parent::withValue($value, $class);
+	}
+	
+	/**
+	 * Answer a new object with the value zero
+	 * 
+	 * @param optional string $class The class to instantiate. Do NOT use outside 
+	 *		of this package.
+	 * @return object Integer
+	 * @access public
+	 * @since 7/14/05
+	 */
+	function &zero ( $class = 'ByteSize') {
+		return parent::zero($class);
+	}
+	
+/*********************************************************
+ * Instance Methods - Printing
+ *********************************************************/
+	
+	/**
+ 	 * Answer a String whose characters are a description of the receiver.
+ 	 * Override this method as needed to provide a better representation
+ 	 * 
+ 	 * @return string
+ 	 * @access public
+ 	 * @since 7/11/05
+ 	 */
+ 	function printableString () {
+		for ($i = 0; $i <= 80; $i = $i + 10) {
+			if ($this->value() < pow(2, ($i + 10)))
+				break;
+		}
+		
+		if ($i == 0)
+			$numString = $this->multipleOfPowerOf2($i);
+		else
+			$numString = sprintf("%01.2f", $this->multipleOfPowerOf2($i));
+			
+		return $numString." ".$this->suffixForPower($i);
+	}
+	
+	/**
+	 * Answer the string in kilo bytes (kB)
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 10/11/05
+	 */
+	function asKBString () {
+		return round($this->multipleOfPowerOf2(10), 2).$this->suffixForPower(10);
+	}
+	
+	/**
+	 * Answer the string in mega bytes (MB)
+	 * 
+	 * @return string
+	 * @access public
+	 * @since 10/11/05
+	 */
+	function asMBString () {
+		return round($this->multipleOfPowerOf2(20), 2).$this->suffixForPower(20);
+	}
+
+/*********************************************************
+ * Instance Methods - Accessing
+ *********************************************************/
+ 	
+ 	/**
+ 	 * Answer the value as a multiple of 2^$power.
+ 	 * Ex: for the number in kilo bytes (in computer-terms), $power = 10;
+ 	 * 
+ 	 * @param integer $power 0 for bytes, 10 for kilobyes, 20 for megabytes, etc.
+ 	 * @return float
+ 	 * @access public
+ 	 * @since 10/11/05
+ 	 */
+ 	function multipleOfPowerOf2 ( $power ) {
+ 		return $this->value() / pow(2, $power);
+ 	}
+
+ 	/**
+ 	 * Answer the value in Kilo Bytes KB
+ 	 * 
+ 	 * @return integer
+ 	 * @access public
+ 	 * @since 10/11/05
+ 	 */
+ 	function kiloBytes () {
+ 		return $this->multipleOfPowerOf2(10);
+ 	}
+
+ 	/**
+ 	 * Answer the value in Mega Bytes (MB)
+ 	 * 
+ 	 * @return integer
+ 	 * @access public
+ 	 * @since 10/11/05
+ 	 */
+ 	function megaBytes () {
+ 		return $this->multipleOfPowerOf2(20);
+ 	}
+ 	
+/*********************************************************
+ * Instance Methods - Private
+ *********************************************************/
+	
+	/**
+	 * Set the internal value to a PHP primitive.
+	 * 
+	 * @param mixed $value
+	 * @return void
+	 * @access private
+	 * @since 7/14/05
+	 */
+	function _setValue ( $value ) {
+		$this->_value = $value;
+	}
+ 	
+}
---
+++ core/Primitives/Numbers/tests/ByteSizeTestCase.class.php
@@ -0,0 +1,125 @@
+<?php
+/** 
+ * @package harmoni.chronology.tests
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: ByteSizeTestCase.class.php,v 1.1 2005/10/11 18:07:21 adamfranco Exp $
+ *
+ * @link http://harmoni.sourceforge.net/
+ * @author Adam Franco <adam AT adamfranco DOT com> <afranco AT middlebury DOT edu>
+ * @since 5/25/05
+ */
+
+require_once(dirname(__FILE__)."/../ByteSize.class.php");
+
+/**
+ * A single unit test case. This class is intended to test one particular
+ * class. Replace 'testedclass.php' below with the class you would like to
+ * test.
+ *
+ * @since 5/25/05
+ *
+ * @package harmoni.chronology.tests
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: ByteSizeTestCase.class.php,v 1.1 2005/10/11 18:07:21 adamfranco Exp $
+ *
+ * @link http://harmoni.sourceforge.net/
+ * @author Adam Franco <adam AT adamfranco DOT com> <afranco AT middlebury DOT edu>
+ */
+
+class ByteSizeTestCase extends UnitTestCase {
+	
+	/**
+	*  Sets up unit test wide variables at the start
+	*	 of each test method.
+	*	 @access public
+	*/
+	function setUp() {
+		$this->bNum =& ByteSize::withValue(280);
+		$this->kbNum =& ByteSize::withValue(2800);
+		$this->mbNum =& ByteSize::withValue(2808093);
+		$this->gbNum =& ByteSize::withValue(8808033932);
+		$this->reallyBigNum =& ByteSize::withValue(80000000000000000000000000);
+	}
+	
+	/**
+	 *	  Clears the data set in the setUp() method call.
+	 *	  @access public
+	 */
+	function tearDown() {
+		// perhaps, unset $obj here
+	}
+	
+	
+	/**
+	 * 
+	 */ 
+	function test_powerOf2() {
+		print "\n<br/>bNum<br/>";
+		$this->assertEqual(round($this->bNum->multipleOfPowerOf2(0), 2), 280);
+		$this->assertEqual(round($this->bNum->multipleOfPowerOf2(10), 2), 0.27);
+		$this->assertEqual(round($this->bNum->multipleOfPowerOf2(20), 2), 0.00);
+		$this->assertEqual(round($this->bNum->multipleOfPowerOf2(30), 2), 0.00);
+		$this->assertEqual(round($this->bNum->multipleOfPowerOf2(40), 2), 0.00);
+		$this->assertEqual(round($this->bNum->multipleOfPowerOf2(50), 2), 0.00);
+		$this->assertEqual(round($this->bNum->multipleOfPowerOf2(60), 2), 0.00);
+		$this->assertEqual(round($this->bNum->multipleOfPowerOf2(70), 2), 0.00);
+		$this->assertEqual(round($this->bNum->multipleOfPowerOf2(80), 2), 0.00);
+		
+		print "\n<br/>kbNum<br/>";
+		$this->assertEqual(round($this->kbNum->multipleOfPowerOf2(0), 2), 2800);
+		$this->assertEqual(round($this->kbNum->multipleOfPowerOf2(10), 2), 2.73);
+		$this->assertEqual(round($this->kbNum->multipleOfPowerOf2(20), 2), 0.00);
+		$this->assertEqual(round($this->kbNum->multipleOfPowerOf2(30), 2), 0.00);
+		$this->assertEqual(round($this->kbNum->multipleOfPowerOf2(40), 2), 0.00);
+		$this->assertEqual(round($this->kbNum->multipleOfPowerOf2(50), 2), 0.00);
+		$this->assertEqual(round($this->kbNum->multipleOfPowerOf2(60), 2), 0.00);
+		$this->assertEqual(round($this->kbNum->multipleOfPowerOf2(70), 2), 0.00);
+		$this->assertEqual(round($this->kbNum->multipleOfPowerOf2(80), 2), 0.00);
+		
+		print "\n<br/>mbNum<br/>";
+		$this->assertEqual(round($this->mbNum->multipleOfPowerOf2(0), 2), 2808093);
+		$this->assertEqual(round($this->mbNum->multipleOfPowerOf2(10), 2), 2742.28);
+		$this->assertEqual(round($this->mbNum->multipleOfPowerOf2(20), 2), 2.68);
+		$this->assertEqual(round($this->mbNum->multipleOfPowerOf2(30), 2), 0.00);
+		$this->assertEqual(round($this->mbNum->multipleOfPowerOf2(40), 2), 0.00);
+		$this->assertEqual(round($this->mbNum->multipleOfPowerOf2(50), 2), 0.00);
+		$this->assertEqual(round($this->mbNum->multipleOfPowerOf2(60), 2), 0.00);
+		$this->assertEqual(round($this->mbNum->multipleOfPowerOf2(70), 2), 0.00);
+		$this->assertEqual(round($this->mbNum->multipleOfPowerOf2(80), 2), 0.00);
+		
+		print "\n<br/>gbNum<br/>";
+		$this->assertEqual(round($this->gbNum->multipleOfPowerOf2(0), 2), 8808033932);
+		$this->assertEqual(round($this->gbNum->multipleOfPowerOf2(10), 2), 8601595.64);
+		$this->assertEqual(round($this->gbNum->multipleOfPowerOf2(20), 2), 8400.00);
+		$this->assertEqual(round($this->gbNum->multipleOfPowerOf2(30), 2), 8.20);
+		$this->assertEqual(round($this->gbNum->multipleOfPowerOf2(40), 2), 0.01);
+		$this->assertEqual(round($this->gbNum->multipleOfPowerOf2(50), 2), 0.00);
+		$this->assertEqual(round($this->gbNum->multipleOfPowerOf2(60), 2), 0.00);
+		$this->assertEqual(round($this->gbNum->multipleOfPowerOf2(70), 2), 0.00);
+		$this->assertEqual(round($this->gbNum->multipleOfPowerOf2(80), 2), 0.00);
+		
+		print "\n<br/>reallybignum<br/>";
+		$this->assertEqual(round($this->reallyBigNum->multipleOfPowerOf2(0), 2), 80000000000000000000000000);
+		$this->assertEqual(round($this->reallyBigNum->multipleOfPowerOf2(40), 2), 72759576141834.27);
+		$this->assertEqual(round($this->reallyBigNum->multipleOfPowerOf2(50), 2), 71054273576.01);
+		$this->assertEqual(round($this->reallyBigNum->multipleOfPowerOf2(60), 2), 69388939.04);
+		$this->assertEqual(round($this->reallyBigNum->multipleOfPowerOf2(70), 2), 67762.64);
+		$this->assertEqual(round($this->reallyBigNum->multipleOfPowerOf2(80), 2), 66.17);
+	}
+	
+	function test_printableString() {
+		print "\n<br/>bNum<br/>";
+		$this->assertEqual($this->bNum->printableString(), '280 B');
+		$this->assertEqual($this->kbNum->printableString(), '2.73 kB');
+		$this->assertEqual($this->mbNum->printableString(), '2.68 MB');
+		$this->assertEqual($this->gbNum->printableString(), '8.20 GB');
+		$this->assertEqual($this->reallyBigNum->printableString(), '66.17 YB');
+	}
+}
+?>
