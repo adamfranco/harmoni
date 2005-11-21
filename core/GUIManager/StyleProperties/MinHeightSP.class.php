ba4b2cabbc76e5977f97e0c1b23535fa56280fdb
adamfranco <adamfranco>
2005-11-21 18:56:47 +0000

    Adding min-height and min-width Style properties.

---
+++ core/GUIManager/StyleProperties/MinHeightSP.class.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once(HARMONI."GUIManager/StyleProperty.class.php");
+require_once(HARMONI."GUIManager/StyleComponents/MaxDimensionSC.class.php");
+
+/**
+ * The MaxHeightSP represents the 'min-height' StyleProperty.
+ * 
+ * A StyleProperty (SP) is one of the tree building pieces of CSS styles. It stores 
+ * information about a single CSS style property by storing one or more 
+ * <code>StyleComponents</code>.
+ * 
+ * The other two CSS styles building pieces are <code>StyleComponents</code> and
+ * <code>StyleCollections</code>. 
+ 
+ *
+ * @package  harmoni.gui.sps
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: MinHeightSP.class.php,v 1.1 2005/11/21 18:56:47 adamfranco Exp $
+ */
+class MinHeightSP extends StyleProperty {
+
+	/**
+	 * The constructor.
+	 * @access public
+	 * @param string value The value of the dimension.
+	 **/
+	function MinHeightSP($value) {
+		$this->StyleProperty("min-height", "Minimum Height", "This property specifies the minimum height.");
+		$this->addSC(new MaxDimensionSC($value));
+	}
+
+}
+
+?>
