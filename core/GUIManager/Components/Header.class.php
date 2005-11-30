00ade79b8db1f0a41853ff3d732e5e2ffbb53da2
adamfranco <adamfranco>
2005-11-30 19:54:11 +0000

    Adding support for a HEADER type for the tops of pages.

---
+++ core/GUIManager/Components/Header.class.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once(HARMONI."GUIManager/Component.class.php");
+
+/**
+ * Header components are identical to Block components with the only exception
+ * that their type is set to <code>HEADER</code>.
+ * <br /><br />
+ * <code>Components</code> are the basic units that can be displayed on
+ * the screen. The main method <code>render()</code> which renders the component 
+ * on the screen.
+ *
+ * @package harmoni.gui.components
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: Header.class.php,v 1.1 2005/11/30 19:54:11 adamfranco Exp $
+ */
+class Header extends Component {
+
+	/**
+	 * The constructor.
+	 * @access public
+	 * @param string content This is an arbitrary string that will be printed,
+	 * whenever the user calls the <code>render()</code> method. The parameter
+	 * is required.
+	 * @param integer index The index of this component. The index has no semantic meaning: 
+	 * you can think of the index as 'level' of the component. Alternatively, 
+	 * the index could serve as means of distinguishing between components with 
+	 * the same type. Most often one would use the index in conjunction with
+	 * the <code>getStylesForComponentType()</code> and 
+	 * <code>addStyleForComponentType()</code> methods.
+	 * @param optional object StyleCollections styles,... Zero, one, or more StyleCollection 
+	 * objects that will be added to the newly created Component. Warning, this will
+	 * result in copying the objects instead of referencing them as using
+	 * <code>addStyle()</code> would do.
+	 **/
+	function Header($content, $index) {
+		// ** parameter validation
+		ArgumentValidator::validate($content,StringValidatorRule::getRule(), true);
+		// ** end of parameter validation	
+
+		$this->Component($content, HEADER, $index);
+		
+		// if there are style collections to add
+		if (func_num_args() > 2)
+			for ($i = 2; $i < func_num_args(); $i++)
+				$this->addStyle(func_get_arg($i));
+	}
+
+}
+
+?>
