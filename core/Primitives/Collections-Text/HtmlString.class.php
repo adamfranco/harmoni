0101ddf80be7f47a06c169854af8a3cc3f516fcf
adamfranco <adamfranco>
2005-12-14 00:21:18 +0000

    Adding HtmlString primitives which support trimming of HTML text while preserving HTML formatting and enforcing the closing of tags.

---
+++ core/Primitives/Collections-Text/HtmlString.class.php
@@ -0,0 +1,187 @@
+<?
+
+require_once(dirname(__FILE__)."/String.class.php");
+
+/**
+ * A HtmlString data type. This class allows for HTML-safe string shortening.
+ *
+ * @package harmoni.datamanager.primitives
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: HtmlString.class.php,v 1.1 2005/12/14 00:21:18 adamfranco Exp $
+ */
+class HtmlString 
+	extends String 
+{
+	var $_children;
+	
+	function HtmlString($string="") {
+		$this->_string = (string) $string;
+	}
+	
+	/**
+	 * Instantiates a new String object with the passed value.
+	 * @param string $value
+	 * @return ref object
+	 * @access public
+	 * @static
+	 */
+	function &withValue($value) {
+		$string =& new HtmlString($value);
+		return $string;
+	}
+	
+	/**
+	 * Shorten the string to a number of words, preserving HTML tags
+	 * while enforcing the closing of html tags.
+	 * 
+	 * @param integer $numWords
+	 * @param boolean $addElipses
+	 * @return void
+	 * @access public
+	 * @since 12/12/05
+	 */
+	function trim ( $numWords, $addElipses = true ) {
+		$tags = array();
+		$wordCount = 0;
+		$output = '';
+		$inWord = false;
+		
+		for ($i=0; $i < strlen($this->_string) && $wordCount < $numWords; $i++) {
+			$char = $this->_string[$i];
+			
+			switch($char) {
+				case '<':
+					// If we are at a tag:
+					//	- 	if we are starting a tag, push that tag onto the tag
+					// 		stack and print it out.
+					//	- 	If we are closing a tag, pop it off of the tag stack.
+					//		and print it out.
+					$tag = $this->getTag($this->_string, $i);
+					$tagHtml = '';
+					$isCloseTag = ($this->_string[$i+1] == '/')?true:false;
+					$isSingleTag = $this->isSingleTag($this->_string, $i);
+					
+// 					print "<hr>Tag: $tag<br/>isCloseTag: ".(($isCloseTag)?'true':'false')."<br/>isSingleTag: ".(($isSingleTag)?'true':'false');
+					
+					// iterate over the tag
+					while ($char != '>') {
+						$char = $this->_string[$i];
+						$i++;
+						$tagHtml .= $char;
+					}
+					$i--; // we've overrun to print the end tag, so decrement $i
+
+					// print "<pre>$tag</pre>"; 
+					
+					if ($isCloseTag) {
+						$topTag = array_pop($tags);
+						$output .= '</'.$topTag.'>';
+					} else if ($isSingleTag) {
+						$output .= $tagHtml;
+					} else {			
+						array_push($tags, $tag);
+						$output .= $tagHtml;
+					}
+					
+					break;
+				case " ":
+				case "\n":
+				case "\r":
+				case "\t":
+					if ($inWord) {
+						$wordCount++;
+						$inWord = false;
+					}
+					$output .= $char;
+					break;
+				default:
+					$inWord = true;
+					$output .= $char;
+			}
+		}
+		
+		// trim off any trailing whitespace
+		$output = trim($output);
+		
+		
+		// If we have text that we aren't printing, print elipses
+		if ($i < strlen($this->_string) && $addElipses) {
+			$output .= dgettext('harmoni', '...');
+		}
+		
+		// if we've hit our word limit and not closed all tags, close them now.
+		while ($tag = array_pop($tags)) {
+			$output .= '</'.$tag.'>';
+		}
+		
+// 		print "<pre>'".htmlspecialchars($output)."'</pre>"; 
+		
+		$this->_string = $output;
+	}
+	
+	/**
+	 * Answer the tag that starts at the given index.
+	 * 
+	 * @param string $inputString
+	 * @param integer $tagStart // index of the opening '<'
+	 * @return string
+	 * @access public
+	 * @since 12/13/05
+	 */
+	function getTag ( $inputString, $tagStart ) {
+		if ($inputString[$tagStart + 1] == '/')
+			$string = substr($inputString, $tagStart + 2);
+		else
+			$string = substr($inputString, $tagStart + 1);
+			
+		$nextSpace = strpos($string, ' ');
+		$nextClose = strpos($string, '>');
+		
+		if ($nextSpace && $nextSpace < $nextClose)
+			$tagEnd = $nextSpace;
+		else
+			$tagEnd = $nextClose;
+			
+		$tag = substr($string, 0, $tagEnd);
+			
+// 		print "<hr>NextSpace: $nextSpace<br/>NextClose: $nextClose<pre>".htmlspecialchars($string)."</pre>"; 
+// 		print "<pre>".htmlspecialchars($tag)."</pre>"; 
+		
+		return $tag;
+	}
+	
+	/**
+	 * Answer true if the tag begining at $tagStart does not have a close-tag,
+	 * examples are <br/>, <hr/>, <img src=''/>
+	 * 
+	 * @param string $inputString
+	 * @param integer $tagStart // index of the opening '<'
+	 * @return string
+	 * @access public
+	 * @since 12/13/05
+	 */
+	function isSingleTag ( $inputString, $tagStart ) {
+		// if this is a close tag itself, return false
+		if ($inputString[$tagStart + 1] == '/')
+			return false;
+		
+		// if this is a tag that ends in '/>', return true
+		$string = substr($inputString, $tagStart + 1);
+		$nextClose = strpos($string, '>');
+		if ($string[$nextClose - 1] == '/')
+			return true;
+		
+		// check the tag to allow exceptions for commonly invalid tags such as
+		// <br>, <hr>, <img src=''>
+		$tag = $this->getTag($inputString, $tagStart);
+		$singleTags = array ('br', 'hr', 'img');
+		if (in_array($tag, $singleTags))
+			return true;
+		
+		// Otherwise
+		return false;
+	}
+}
---
+++ core/Primitives/Collections-Text/tests/HtmlStringTestCase.class.php
@@ -0,0 +1,307 @@
+<?php
+/** 
+ * @package harmoni.chronology.tests
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: HtmlStringTestCase.class.php,v 1.1 2005/12/14 00:21:18 adamfranco Exp $
+ *
+ * @link http://harmoni.sourceforge.net/
+ * @author Adam Franco <adam AT adamfranco DOT com> <afranco AT middlebury DOT edu>
+ * @since 5/3/05
+ */
+
+require_once(dirname(__FILE__)."/../HtmlString.class.php");
+
+/**
+ * A single unit test case. This class is intended to test one particular
+ * class. Replace 'testedclass.php' below with the class you would like to
+ * test.
+ *
+ * @since 5/3/05
+ *
+ * @package harmoni.chronology.tests
+ * 
+ * @copyright Copyright &copy; 2005, Middlebury College
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
+ *
+ * @version $Id: HtmlStringTestCase.class.php,v 1.1 2005/12/14 00:21:18 adamfranco Exp $
+ *
+ * @link http://harmoni.sourceforge.net/
+ * @author Adam Franco <adam AT adamfranco DOT com> <afranco AT middlebury DOT edu>
+ */
+
+class HtmlStringTestCase extends UnitTestCase {
+	
+	/**
+	*  Sets up unit test wide variables at the start
+	*	 of each test method.
+	*	 @access public
+	*/
+	function setUp() {
+		
+	}
+	
+	/**
+	 *	  Clears the data set in the setUp() method call.
+	 *	  @access public
+	 */
+	function tearDown() {
+		// perhaps, unset $obj here
+	}
+	
+	/**
+	 * Test the creation methods.
+	 */ 
+	function test_trim_tag_handling() {
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(100);
+		$this->assertEqual($htmlString->asString(), $string);
+		
+		// test single html tags <hr/>
+		$string = 
+"Hello world.<hr/>
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(100);
+		$this->assertEqual($htmlString->asString(), $string);
+		
+		$string = 
+"Hello world.<img src='' border='1' />
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(100);
+		$this->assertEqual($htmlString->asString(), $string);
+		
+		// Test bad html tags.
+		$string = 
+"Hello world.<hr> <img src='' border='1'>
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(100);
+		$this->assertEqual($htmlString->asString(), $string);
+		
+		// test re-nesting
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong><em>fox</strong></em> 
+jumped over the lazy <em>dog</em>.</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong><em>fox</em></strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$htmlString->trim(100);
+		$this->assertEqual($htmlString->asString(), $string);
+	}
+	
+	/**
+	 * Test the creation methods.
+	 */ 
+	function test_trim_lengths() {
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello...";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(1);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world....";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(2);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello   \n \n\t \n\r  world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello   \n \n\t \n\r  world....";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(2);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The...</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(3);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The quick...</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(4);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The quick brown...</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(5);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong>...</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(6);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped...</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(7);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over...</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(8);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the...</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(9);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy...</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(10);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(11);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(12);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(13);
+		$this->assertEqual($htmlString->asString(), $result);
+	}
+	
+	/**
+	 * Test Elipses
+	 */
+	function test_elipses () {
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world....";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(2);
+		$this->assertEqual($htmlString->asString(), $result);
+		
+		$string = 
+"Hello world.
+<p style='font-size: large;'>The quick brown <strong>fox</strong> 
+jumped over the lazy <em>dog</em>.</p>";
+		$result = 
+"Hello world.";
+		$htmlString =& HtmlString::withValue($string);
+		$htmlString->trim(2, false);
+		$this->assertEqual($htmlString->asString(), $result);
+	}
+}
+?>
