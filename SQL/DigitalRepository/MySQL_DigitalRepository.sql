aa972aef36b526ee8b5c755aa9b7784f11877760
adamfranco <adamfranco>
2006-05-04 18:58:58 +0000

    Updating SQL file to include Authority-List tables.

--- SQL/DigitalRepository/MySQL_DigitalRepository.sql
+++ SQL/DigitalRepository/MySQL_DigitalRepository.sql
@@ -4,7 +4,7 @@
 -- @copyright Copyright &copy; 2005, Middlebury College
 -- @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
 --
--- @version $Id: MySQL_DigitalRepository.sql,v 1.12 2006/02/13 22:53:43 adamfranco Exp $
+-- @version $Id: MySQL_DigitalRepository.sql,v 1.13 2006/05/04 18:58:58 adamfranco Exp $
 -- */
 -- --------------------------------------------------------
 
@@ -147,3 +147,28 @@ CREATE TABLE dr_resized_cache (
   `data` longblob NOT NULL,
   PRIMARY KEY  (FK_file,size,websafe)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This table is use by polyphony.modules.repository';
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `dr_authoritative_values`
+-- 
+
+CREATE TABLE dr_authoritative_values (
+  fk_partstructure varchar(100) NOT NULL default '',
+  `value` varchar(240) NOT NULL default '',
+  PRIMARY KEY  (fk_partstructure,`value`),
+  KEY fk_partstructure (fk_partstructure)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This table is used by the part structure to maintain authori';
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `dr_authority_options`
+-- 
+
+CREATE TABLE dr_authority_options (
+  fk_partstructure varchar(100) NOT NULL default '',
+  user_addition_allowed tinyint(1) NOT NULL default '0',
+  KEY fk_partstructure (fk_partstructure)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This table is used by the part structure to maintain authori';
