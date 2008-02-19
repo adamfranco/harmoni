f84bced56992702ed961dc64a96f6d70b721dafc
adamfranco <adamfranco>
2008-02-19 21:43:20 +0000

    AuthZ: Fixed issue in default Type constuctors.

--- core/oki2/authorization/DefaultFunctionType.class.php
+++ core/oki2/authorization/DefaultFunctionType.class.php
@@ -10,15 +10,15 @@ require_once(HARMONI."oki2/shared/HarmoniType.class.php");
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: DefaultFunctionType.class.php,v 1.6 2007/04/12 15:37:27 adamfranco Exp $
+ * @version $Id: DefaultFunctionType.class.php,v 1.7 2008/02/19 21:43:20 adamfranco Exp $
  */
 
 class DefaultFunctionType 
 	extends HarmoniType 
 {
 
-	function DefaultFunctionType() {
-		$this->HarmoniType("harmoni", "authorization", "function");
+	function __construct() {
+		parent::__construct("harmoni", "authorization", "function");
 	}
 
 }
--- core/oki2/authorization/DefaultQualifierType.class.php
+++ core/oki2/authorization/DefaultQualifierType.class.php
@@ -11,14 +11,14 @@ require_once(HARMONI."oki2/shared/HarmoniType.class.php");
  * @copyright Copyright &copy; 2005, Middlebury College
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
  *
- * @version $Id: DefaultQualifierType.class.php,v 1.7 2007/04/12 15:37:27 adamfranco Exp $
+ * @version $Id: DefaultQualifierType.class.php,v 1.8 2008/02/19 21:43:20 adamfranco Exp $
  */
 class DefaultQualifierType 
 	extends HarmoniType 
 {
 
-	function DefaultQualifierType() {
-		$this->HarmoniType("authorization", "edu.middlebury.harmoni", "qualifier");
+	function __construct() {
+		parent::__construct("authorization", "edu.middlebury.harmoni", "qualifier");
 	}
 
 }
