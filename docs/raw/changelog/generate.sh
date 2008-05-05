d1242a9b833e4b535d03b091d28ebb9b9ac66895
Adam Franco <afranco@middlebury.edu>
2008-05-05 13:08:12 -0400

    Updates to Release Notes XSL transforms.
    A proper 'release notes' and README.txt file is now generated from the changelog.xml
    source with nicer formatting and more general links and information.
    
    Also, entity escaping is now disabled for the text-output versions
    of the release notes and change log.

--- docs/raw/changelog/generate.sh
+++ docs/raw/changelog/generate.sh
@@ -5,7 +5,7 @@
 #  @copyright Copyright &copy; 2005, Middlebury College
 #  @license http://www.gnu.org/copyleft/gpl.html GNU General Public License (GPL)
 #  
-#  @version $Id: generate.sh,v 1.5 2007/10/09 18:39:03 adamfranco Exp $
+#  @version $Id: generate.sh,v 1.2 2007/10/09 18:47:56 adamfranco Exp $
 
 progdir=`dirname $0`
 cd $progdir
@@ -13,4 +13,5 @@ cd $progdir
 xsltproc ../xslt/changelog-simplehtml.xsl changelog.xml | sed -e 's/<?xml.*?>//' > ../../changelog.html
 xsltproc ../xslt/changelog-plaintext.xsl changelog.xml | sed -e 's/<?xml.*?>//' > ../../changelog.txt
 xsltproc ../xslt/releaseNotes-plaintext.xsl changelog.xml | sed -e 's/<?xml.*?>//' > ../../release_notes.txt
+xsltproc ../xslt/releaseNotes-plaintext.xsl changelog.xml | sed -e 's/<?xml.*?>//' > ../../../README.txt
 
