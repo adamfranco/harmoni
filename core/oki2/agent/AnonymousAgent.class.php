c6afb6075591315d1621616f7b308ac8a56c5dc5
Adam Franco <afranco@middlebury.edu>
2009-07-24 10:31:57 -0400

    Updated the AnonymousAgent class to be operational.
    It appears not to have been used in a long time and no longer aligned with
    the HarmoniAgent parent class. It is now just a lightweight container.

--- core/oki2/agent/AnonymousAgent.class.php
+++ core/oki2/agent/AnonymousAgent.class.php
@@ -27,23 +27,183 @@ class AnonymousAgent
 	 * @access public
 	 * @since 2/8/05
 	 */
-	function AnonymousAgent ($dbIndex) {
+	function __construct () {
 		$idManager = Services::getService("Id");
-		$id =$idManager->getId("edu.middlebury.agents.anonymous");
+		$this->id = $idManager->getId("edu.middlebury.agents.anonymous");
 		
-		$type = new Type("Agents", "edu.middlebury.harmoni", "Any/Anonymous", 
+		$this->type = new Type("Agents", "edu.middlebury.harmoni", "Any/Anonymous", 
 			_("Special users that can represent anyone or unknown users."));
 		
-		$propertiesArray = array();
-// 		$propertiesType = new HarmoniType('Agents', 'Harmoni', 'Agent Properties',
-// 						'Properties known to the Harmoni Agents System.');
-// 		$propertiesArray[0] = new HarmoniProperties($propertiesType);
-		
-		$this->HarmoniAgent(	_("Anonymous"),
-								$id,
-								$type,
-								$propertiesArray,
-								$dbIndex);
+		$this->propertiesArray = array();
+	}
+	
+	
+	
+	/**
+	 * Get the name of this Agent.
+	 *	
+	 * @return string
+	 * 
+	 * @throws object AgentException An exception with one of the
+	 *		   following messages defined in org.osid.agent.AgentException may
+	 *		   be thrown:  {@link
+	 *		   org.osid.agent.AgentException#OPERATION_FAILED
+	 *		   OPERATION_FAILED}, {@link
+	 *		   org.osid.agent.AgentException#PERMISSION_DENIED
+	 *		   PERMISSION_DENIED}, {@link
+	 *		   org.osid.agent.AgentException#CONFIGURATION_ERROR
+	 *		   CONFIGURATION_ERROR}, {@link
+	 *		   org.osid.agent.AgentException#UNIMPLEMENTED UNIMPLEMENTED}
+	 * 
+	 * @access public
+	 */
+	function getDisplayName () { 
+		return _("Anonymous");
+	}
+
+	/**
+	 * Get the id of this Agent.
+	 *	
+	 * @return object Id
+	 * 
+	 * @throws object AgentException An exception with one of the
+	 *		   following messages defined in org.osid.agent.AgentException may
+	 *		   be thrown:  {@link
+	 *		   org.osid.agent.AgentException#OPERATION_FAILED
+	 *		   OPERATION_FAILED}, {@link
+	 *		   org.osid.agent.AgentException#PERMISSION_DENIED
+	 *		   PERMISSION_DENIED}, {@link
+	 *		   org.osid.agent.AgentException#CONFIGURATION_ERROR
+	 *		   CONFIGURATION_ERROR}, {@link
+	 *		   org.osid.agent.AgentException#UNIMPLEMENTED UNIMPLEMENTED}
+	 * 
+	 * @access public
+	 */
+	function getId () { 
+		return $this->id;
+	}
+
+	/**
+	 * Get the type of this Agent.
+	 *	
+	 * @return object Type
+	 * 
+	 * @throws object AgentException An exception with one of the
+	 *		   following messages defined in org.osid.agent.AgentException may
+	 *		   be thrown:  {@link
+	 *		   org.osid.agent.AgentException#OPERATION_FAILED
+	 *		   OPERATION_FAILED}, {@link
+	 *		   org.osid.agent.AgentException#PERMISSION_DENIED
+	 *		   PERMISSION_DENIED}, {@link
+	 *		   org.osid.agent.AgentException#CONFIGURATION_ERROR
+	 *		   CONFIGURATION_ERROR}, {@link
+	 *		   org.osid.agent.AgentException#UNIMPLEMENTED UNIMPLEMENTED}
+	 * 
+	 * @access public
+	 */
+	function getType () { 
+		return $this->type;
+	}
+
+	/**
+	 * Get the Properties associated with this Agent.
+	 *	
+	 * @return object PropertiesIterator
+	 * 
+	 * @throws object AgentException An exception with one of the
+	 *		   following messages defined in org.osid.agent.AgentException may
+	 *		   be thrown:  {@link
+	 *		   org.osid.agent.AgentException#OPERATION_FAILED
+	 *		   OPERATION_FAILED}, {@link
+	 *		   org.osid.agent.AgentException#PERMISSION_DENIED
+	 *		   PERMISSION_DENIED}, {@link
+	 *		   org.osid.agent.AgentException#CONFIGURATION_ERROR
+	 *		   CONFIGURATION_ERROR}, {@link
+	 *		   org.osid.agent.AgentException#UNIMPLEMENTED UNIMPLEMENTED}
+	 * 
+	 * @access public
+	 */
+	function getProperties () { 
+		return new HarmoniProperties(new HarmoniType('Agents', 'Harmoni', 'Agent Properties',
+ 						'Properties known to the Harmoni Agents System.'));
+	}
+
+	/**
+	 * Get the Properties of this Type associated with this Agent.
+	 * 
+	 * @param object Type $propertiesType
+	 *	
+	 * @return object Properties
+	 * 
+	 * @throws object AgentException An exception with one of the
+	 *		   following messages defined in org.osid.agent.AgentException may
+	 *		   be thrown:  {@link
+	 *		   org.osid.agent.AgentException#OPERATION_FAILED
+	 *		   OPERATION_FAILED}, {@link
+	 *		   org.osid.agent.AgentException#PERMISSION_DENIED
+	 *		   PERMISSION_DENIED}, {@link
+	 *		   org.osid.agent.AgentException#CONFIGURATION_ERROR
+	 *		   CONFIGURATION_ERROR}, {@link
+	 *		   org.osid.agent.AgentException#UNIMPLEMENTED UNIMPLEMENTED},
+	 *		   {@link org.osid.agent.AgentException#NULL_ARGUMENT
+	 *		   NULL_ARGUMENT}, {@link
+	 *		   org.osid.agent.AgentException#UNKNOWN_TYPE UNKNOWN_TYPE}
+	 * 
+	 * @access public
+	 */
+	function getPropertiesByType ( Type $propertiesType ) { 
+		return new HarmoniProperties($propertiesType);
+	}
+
+	/**
+	 * Get the Properties Types supported by this Agent.
+	 *	
+	 * @return object TypeIterator
+	 * 
+	 * @throws object AgentException An exception with one of the
+	 *		   following messages defined in org.osid.agent.AgentException may
+	 *		   be thrown:  {@link
+	 *		   org.osid.agent.AgentException#OPERATION_FAILED
+	 *		   OPERATION_FAILED}, {@link
+	 *		   org.osid.agent.AgentException#PERMISSION_DENIED
+	 *		   PERMISSION_DENIED}, {@link
+	 *		   org.osid.agent.AgentException#CONFIGURATION_ERROR
+	 *		   CONFIGURATION_ERROR}, {@link
+	 *		   org.osid.agent.AgentException#UNIMPLEMENTED UNIMPLEMENTED}
+	 * 
+	 * @access public
+	 */
+	function getPropertyTypes () { 
+		$array = array();
+		$iterator = new HarmoniIterator($array);
+		return $iterator;
+	
+	}
+	
+	/**
+	 * Answer true if this Agent is an Agent, not a group
+	 *
+	 * WARNING: NOT IN OSID
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 12/7/06
+	 */
+	function isAgent () {
+		return !$this->isGroup();
+	}
+	
+	/**
+	 * Answer true if this Agent is an Group
+	 *
+	 * WARNING: NOT IN OSID
+	 * 
+	 * @return boolean
+	 * @access public
+	 * @since 12/7/06
+	 */
+	function isGroup () {
+		return false;
 	}
 }
 
